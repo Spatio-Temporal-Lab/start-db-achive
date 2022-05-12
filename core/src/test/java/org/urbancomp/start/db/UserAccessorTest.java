1842cdb5ef617f09a5804e9a5cbe4a1807cc2a1e
jimo <wangpeng417@jd.com>
2022-05-12 23:42:17 +0800

    use mysql image; build hbase image

--- core/src/test/java/org/urbancomp/start/db/UserAccessorTest.java
+++ core/src/test/java/org/urbancomp/start/db/UserAccessorTest.java
@@ -1,41 +1,12 @@
 package org.urbancomp.start.db;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.urbcomp.start.db.metadata.AccessorFactory;
 import org.urbcomp.start.db.metadata.accessor.UserAccessor;
-import org.urbcomp.start.db.util.ResourceUtil;
-
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.util.Properties;
-import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertTrue;
 
 public class UserAccessorTest {
-
-    @BeforeClass
-    public static void init() throws Exception {
-        final Properties p = new Properties();
-        try (final InputStream in = ResourceUtil.readResource("config.properties")) {
-            p.load(in);
-        }
-
-        final String sqlTxt = Files.readAllLines(Paths.get("src/main/resources/metadata/ddl.sql"))
-                .stream().filter(line -> !line.trim().startsWith("#") && !line.trim().startsWith("--")).collect(Collectors.joining());
-        final String[] sqlList = sqlTxt.split(";");
-        try (Connection conn = DriverManager.getConnection(p.getProperty("meta.jdbc.url"))) {
-            for (String sql : sqlList) {
-                final Statement stat = conn.createStatement();
-                stat.execute(sql.trim());
-            }
-        }
-    }
 
     @Test
     public void userTest() {
--- test/embedded-mysql/src/main/java/org/urbcomp/start/db/test/MySQLServerInit.java
+++ test/embedded-mysql/src/main/java/org/urbcomp/start/db/test/MySQLServerInit.java
@@ -1,25 +1,36 @@
 package org.urbcomp.start.db.test;
 
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.utility.DockerImageName;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.stream.Collectors;
 
 /**
- * need docker env
+ * init mysql schema and data
  *
  * @author jimo
  * @date 2022/5/11 7:31
  **/
 public class MySQLServerInit {
 
-    private final static DockerImageName MYSQL_57_IMAGE = DockerImageName.parse("mysql:5.7.34");
+    public static void main(String[] args) throws Exception {
+        init();
+    }
 
-    public static void main(String[] args) {
-        MySQLContainer<?> mysql = new MySQLContainer<>(MYSQL_57_IMAGE)
-                .withUsername("start_db")
-                .withPassword("start_db")
-                .withInitScript("metadata/ddl.sql")
-                .withDatabaseName("meta_data");
-
-        mysql.start();
+    public static void init() throws Exception {
+        String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/meta_data?useSSL=false&useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT+8";
+        String user = "start_db";
+        String password = "start_db";
+        final String sqlTxt = Files.readAllLines(Paths.get("test/embedded-mysql/src/main/resources/metadata/ddl.sql"))
+                .stream().filter(line -> !line.trim().startsWith("#") && !line.trim().startsWith("--")).collect(Collectors.joining());
+        final String[] sqlList = sqlTxt.split(";");
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, user, password)) {
+            for (String sql : sqlList) {
+                final Statement stat = conn.createStatement();
+                stat.execute(sql.trim());
+            }
+        }
     }
 }
