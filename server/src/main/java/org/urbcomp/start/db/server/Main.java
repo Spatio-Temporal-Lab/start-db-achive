4990fe83432468da8ce1c7efd4abcc7fc7fc468e
jimolonely <wp147188@foxmail.com>
2022-05-23 21:56:01 +0800

    add server module

---
+++ server/src/main/java/org/urbcomp/start/db/server/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2022 ST-Lab
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+package org.urbcomp.start.db.server;
+
+import org.apache.calcite.avatica.server.AvaticaJsonHandler;
+import org.apache.calcite.avatica.server.HttpServer;
+import org.apache.calcite.jdbc.DbMetaFactory;
+
+/**
+ * @author jimo
+ **/
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String[] params = {DbMetaFactory.class.getCanonicalName()};
+        final HttpServer server = org.apache.calcite.avatica.server.Main.start(params, 8000, AvaticaJsonHandler::new);
+        server.join();
+    }
+}
