d44121070b0ab5f3331c9dbb6678f5effe91c474
cquhx <48610039+cquhx@users.noreply.github.com>
2022-05-30 21:17:02 +0800

    implement StringFunction.java and add corresponding tests

--- start-db-core/src/test/scala/org/urbcomp/start/db/StringFunctionTest.scala
+++ start-db-core/src/test/scala/org/urbcomp/start/db/StringFunctionTest.scala
@@ -13,8 +13,12 @@ package org.urbcomp.start.db
 
 import org.junit.Assert.assertEquals
 
+/**
+ * String Function test
+ * @author zaiyuan, XiangHe
+ */
+
 class StringFunctionTest extends CalciteGeomesaFunctionTest {
-  // TODO by Xiang He
   test("concat") {
     val statement = connect.createStatement
     val resultSet = statement.executeQuery("select concat('1', '2')")
@@ -28,4 +32,119 @@ class StringFunctionTest extends CalciteGeomesaFunctionTest {
     resultSet.next()
     assertEquals("edcba", resultSet.getObject(1))
   }
+
+  test("upper") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select upper('abcde')")
+    resultSet.next()
+    assertEquals("ABCDE", resultSet.getObject(1))
+  }
+
+  test("lower") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select lower('ABCDE')")
+    resultSet.next()
+    assertEquals("abcde", resultSet.getObject(1))
+  }
+
+  test("substring1") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select substring('abcde', 2, 5)")
+    resultSet.next()
+    assertEquals("bcde", resultSet.getObject(1))
+  }
+
+  test("substring2") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select substring('abcde', 2)")
+    resultSet.next()
+    assertEquals("bcde", resultSet.getObject(1))
+  }
+
+  test("trim") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select trim('  abcde ')")
+    resultSet.next()
+    assertEquals("abcde", resultSet.getObject(1))
+  }
+
+  test("ltrim") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select ltrim('  abcde ')")
+    resultSet.next()
+    assertEquals("abcde ", resultSet.getObject(1))
+  }
+
+  test("rtrim") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select rtrim('  abcde ')")
+    resultSet.next()
+    assertEquals("  abcde", resultSet.getObject(1))
+  }
+
+  test("lpad1") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select lpad('abcde', 2)")
+    resultSet.next()
+    assertEquals("  abcde", resultSet.getObject(1))
+  }
+
+  test("lpad2") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select lpad('abcde', 2, 'a')")
+    resultSet.next()
+    assertEquals("aaabcde", resultSet.getObject(1))
+  }
+
+  test("rpad1") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select rpad('abcde', 1)")
+    resultSet.next()
+    assertEquals("abcde ", resultSet.getObject(1))
+  }
+
+  test("rpad2") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select rpad('abcde', 1, 'e')")
+    resultSet.next()
+    assertEquals("abcdee", resultSet.getObject(1))
+  }
+
+  //TODO 多字节字符测试
+  test("length") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select length('abc')")
+    resultSet.next()
+    assertEquals(3, resultSet.getObject(1))
+  }
+
+  //TODO 多字节字符测试
+  test("charLength") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select charLength('abc')")
+    resultSet.next()
+    assertEquals(3, resultSet.getObject(1))
+  }
+
+  test("locate1") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select locate('bc', 'abcabc')")
+    resultSet.next()
+    assertEquals(2, resultSet.getObject(1))
+  }
+
+  test("locate2") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select locate('bc', 'abcabc', 2)")
+    resultSet.next()
+    assertEquals(5, resultSet.getObject(1))
+  }
+
+  test("md5") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery("select md5('abcde')")
+    resultSet.next()
+    assertEquals("AB56B4D92B40713ACC5AF89985D4B786", resultSet.getObject(1))
+  }
+
 }
