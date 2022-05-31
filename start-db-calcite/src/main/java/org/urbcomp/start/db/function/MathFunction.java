cbb749dd932cd0ca9a11295255a05679cd055281
LI_Or <1519316637@qq.com>
2022-05-31 18:15:13 +0800

    MathFunction.java 初步完成

--- start-db-calcite/src/main/java/org/urbcomp/start/db/function/MathFunction.java
+++ start-db-calcite/src/main/java/org/urbcomp/start/db/function/MathFunction.java
@@ -22,12 +22,15 @@
 
 package org.urbcomp.start.db.function;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class MathFunction {
     /**
-     * Returns the natural logarithm (base) of a double value (num).
+     * Returns the (base) logarithm of a double value (num).
      *
-     * @param base base double
-     * @param num  num double
+     * @param base double
+     * @param num  double
      * @return log result
      */
     @StartDBFunction("log")
@@ -45,91 +48,219 @@ public class MathFunction {
         return Math.PI;
     }
 
+    /**
+     * Returns the base 2 logarithm of a double value (num).
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("log2")
     public double log2(double num) {
         return Math.log(num) / Math.log(2.0D);
     }
 
+    /**
+     * Returns the natural logarithm of a double value (num) + 1.
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("log1p")
     public double log1p(double num) {
         return Math.log1p(num);
     }
 
+    /**
+     * Returns the natural logarithm of a double value (num)
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("ln")
     public double ln(double num) {
         return Math.log(num);
     }
 
+    /**
+     * Returns the base 10 logarithm of a double value (num).
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("log10")
     public double log10(double num) {
         return Math.log10(num);
     }
 
+    /**
+     * Returns the absolute value of an int value.
+     *
+     * @param num int
+     * @return int
+     */
     @StartDBFunction("abs")
     public int abs(int num) {
         return (num < 0) ? -num : num;
     }
 
+    /**
+     * Returns the absolute value of a long value.
+     *
+     * @param num long
+     * @return long
+     */
     @StartDBFunction("abs")
     public long abs(long num) {
         return (num < 0) ? -num : num;
     }
 
+    /**
+     * Returns the absolute value of a float value.
+     *
+     * @param num float
+     * @return float
+     */
     @StartDBFunction("abs")
     public float abs(float num) {
         return (num <= 0.0F) ? 0.0F - num : num;
     }
 
+    /**
+     * Returns the absolute value of a double value.
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("abs")
     public double abs(double num) {
         return (num <= 0.0D) ? 0.0D - num : num;
     }
 
+    /**
+     * Returns the smallest (closest to negative infinity) double value that is
+     * greater than or equal to the argument and is equal to a mathematical integer.
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("ceil")
     public double ceil(double num) {
         return Math.ceil(num);
     }
 
+    /**
+     * Returns the largest (closest to positive infinity) double value that is
+     * less than or equal to the argument and is equal to a mathematical integer.
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("floor")
     public double floor(double num) {
         return Math.floor(num);
     }
 
+    /**
+     * Returns the rounded value of the precision(num_digits)
+     *
+     * @param num        double
+     * @param num_digits int
+     * @return double
+     */
+    @StartDBFunction("round")
+    public double round(double num, int num_digits) {
+        BigDecimal bd = new BigDecimal(num);
+
+        return bd.setScale(num_digits, RoundingMode.HALF_UP).doubleValue();
+    }
+
+    /**
+     * Returns the signum function of the argument; zero if the argument is zero,
+     * 1.0f if the argument is greater than zero, -1.0f if the argument is less than zero.
+     *
+     * @param num float
+     * @return float
+     */
     @StartDBFunction("signum")
     public float signum(float num) {
         return Math.signum(num);
     }
 
+    /**
+     * Returns the signum function of the argument; zero if the argument is zero,
+     * 1.0f if the argument is greater than zero, -1.0f if the argument is less than zero.
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("signum")
     public double signum(double num) {
         return Math.signum(num);
     }
 
+    /**
+     * Returns the floor modulus of the int arguments.
+     * x%y
+     *
+     * @param num1 int x
+     * @param num2 int y
+     * @return int
+     */
     @StartDBFunction("mod")
     public int mod(int num1, int num2) {
         return Math.floorMod(num1, num2);
     }
 
+    /**
+     * Returns the trigonometric sine of an angle.
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("sin")
     public double sin(double num) {
         return Math.sin(num);
     }
 
+    /**
+     * Returns the trigonometric cosine of an angle.
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("cos")
     public double cos(double num) {
         return Math.cos(num);
     }
 
+    /**
+     * Returns the trigonometric tangent of an angle.
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("tan")
     public double tan(double num) {
         return Math.tan(num);
     }
 
+    /**
+     * Returns the arc sine of a value; the returned angle is in the range -pi/2 through pi/2.
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("asin")
     public double asin(double num) {
         return Math.asin(num);
     }
 
+    /**
+     * Returns the arc cosine of a value; the returned angle is in the range -pi/2 through pi/2.
+     *
+     * @param num double
+     * @return double
+     */
     @StartDBFunction("acos")
     public double acos(double num) {
         return Math.acos(num);
