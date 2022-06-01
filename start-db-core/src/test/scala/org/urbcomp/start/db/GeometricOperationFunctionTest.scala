54d6aaaa095b8bf73b0e3ed40e6d35900da7d1d6
Leo <liruiyuanwhu@qq.com>
2022-06-01 16:17:40 +0800

    GeometricOperationFunction added

--- start-db-core/src/test/scala/org/urbcomp/start/db/GeometricOperationFunctionTest.scala
+++ start-db-core/src/test/scala/org/urbcomp/start/db/GeometricOperationFunctionTest.scala
@@ -26,16 +26,18 @@ class GeometricOperationFunctionTest extends CalciteGeomesaFunctionTest {
 
   test("st_x(point)") {
     val statement = connect.createStatement
-    val resultSet = statement.executeQuery("select st_x(st_makePoint(1, 2))")
+    val resultSet = statement.executeQuery("select st_x(st_makePoint(1, 2)), st_x(st_makeBBox(1, 2, 3, 4))")
     resultSet.next()
     assertEquals("1.0", resultSet.getObject(1).toString)
+    assertEquals(null, resultSet.getObject(2))
   }
 
   test("st_y(point)") {
     val statement = connect.createStatement
-    val resultSet = statement.executeQuery("select st_y(st_makePoint(1, 2))")
+    val resultSet = statement.executeQuery("select st_y(st_makePoint(1, 2)), st_y(st_makeBBox(1, 2, 3, 4))")
     resultSet.next()
     assertEquals("2.0", resultSet.getObject(1).toString)
+    assertEquals(null, resultSet.getObject(2))
   }
 
   test("st_BBox(geom)") {
@@ -45,5 +47,63 @@ class GeometricOperationFunctionTest extends CalciteGeomesaFunctionTest {
     resultSet.next()
     assertEquals("POINT (1 2)", resultSet.getObject(1).toString)
     assertEquals("POLYGON ((1 2, 1 4, 3 4, 3 2, 1 2))", resultSet.getObject(2).toString)
+  }
+
+  test("st_numPoints(geom)") {
+    val statement = connect.createStatement
+    val resultSet =
+      statement.executeQuery("select st_numPoints(st_makePoint(1, 2)), st_numPoints(st_makeBBox(1, 2, 3, 4))")
+    resultSet.next()
+    assertEquals("1", resultSet.getObject(1).toString)
+    assertEquals("5", resultSet.getObject(2).toString)
+  }
+
+  test("st_pointN(geom, n)") {
+    val statement = connect.createStatement
+    val resultSet =
+      statement.executeQuery("select st_pointN(st_makePoint(1, 2), 1), st_pointN(st_makeBBox(1, 2, 3, 4), 1)")
+    resultSet.next()
+    assertEquals(null, resultSet.getObject(1))
+    assertEquals(null, resultSet.getObject(2))
+    // todo 测试LineString的结果
+  }
+
+  test("st_area(geom)") {
+    val statement = connect.createStatement
+    val resultSet =
+      statement.executeQuery("select st_area(st_makePoint(1, 2)), st_area(st_makeBBox(1, 2, 3, 4))")
+    resultSet.next()
+    assertEquals(0.0, resultSet.getObject(1))
+    assertEquals(4.0, resultSet.getObject(2))
+  }
+
+  test("st_centroid(geom)") {
+    val statement = connect.createStatement
+    val resultSet =
+      statement.executeQuery("select st_centroid(st_makePoint(1, 2)), st_centroid(st_makeBBox(1, 2, 3, 4))")
+    resultSet.next()
+    assertEquals("POINT (1 2)", resultSet.getObject(1).toString)
+    assertEquals("POINT (2 3)", resultSet.getObject(2).toString)
+  }
+
+  test("st_closestPoint(geom1, geom2)") {
+    val statement = connect.createStatement
+    val resultSet =
+      statement.executeQuery("select st_closestPoint(st_makePoint(1, 2), st_makeBBox(1, 2, 3, 4))")
+    resultSet.next()
+    assertEquals("POINT (1 2)", resultSet.getObject(1).toString)
+  }
+
+  test("st_distance(geom1, geom2)") {
+    val statement = connect.createStatement
+    val resultSet =
+      statement.executeQuery("select st_distance(st_makePoint(1, 2), st_makeBBox(1, 2, 3, 4)), st_distance(st_makePoint(0, 0), st_makeBBox(1, 2, 3, 4))")
+    resultSet.next()
+    assertEquals(0.0, resultSet.getObject(1))
+    assertEquals(Math.sqrt(5), resultSet.getObject(2))
+  }
+
+  test("st_distanceSphere(geom1, geom2)") {
+    // todo
   }
 }
