7baaac39d6dc4e2517d537013f52e50d1bf5dac8
beecorn <bohong_wang@hotmail.com>
2022-06-04 20:29:18 +0800

    fix bugs

--- start-db-calcite/src/main/java/org/apache/calcite/sql/type/SqlTypeAssignmentRule.java
+++ start-db-calcite/src/main/java/org/apache/calcite/sql/type/SqlTypeAssignmentRule.java
@@ -201,7 +201,7 @@ public class SqlTypeAssignmentRule implements SqlTypeMappingRule {
         rules.add(SqlTypeName.POLYGON, EnumSet.of(SqlTypeName.GEOMETRY));
         rules.add(SqlTypeName.MULTIPOLYGON, EnumSet.of(SqlTypeName.GEOMETRY));
         rules.add(SqlTypeName.GEOMETRYCOLLECTION, EnumSet.of(SqlTypeName.GEOMETRY));
-        rules.add(SqlTypeName.DATETIME, EnumSet.of(SqlTypeName.GEOMETRY));
+        rules.add(SqlTypeName.DATETIME, EnumSet.of(SqlTypeName.DATETIME));
         // start-db add end
 
         // ARRAY is assignable from ...
