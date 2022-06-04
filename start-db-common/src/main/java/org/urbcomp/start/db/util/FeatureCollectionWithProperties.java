5461a721837adf5b9d5e795cb32de55785dc9fbd
Leo <liruiyuanwhu@qq.com>
2022-06-05 00:27:04 +0800

    add trajectory togeojson

--- start-db-common/src/main/java/org/urbcomp/start/db/util/FeatureCollectionWithProperties.java
+++ start-db-common/src/main/java/org/urbcomp/start/db/util/FeatureCollectionWithProperties.java
@@ -1,22 +1,35 @@
+/*
+ * Copyright 2022 ST-Lab
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
 package org.urbcomp.start.db.util;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import org.geojson.FeatureCollection;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+// Here this class disguise itself as feature collection
+@JsonTypeName(value = "FeatureCollection")
 public class FeatureCollectionWithProperties extends FeatureCollection {
-    @JsonInclude(JsonInclude.Include.ALWAYS)
     private Map<String, Object> properties = new HashMap<String, Object>();
+
     public void setProperty(String key, Object value) {
         properties.put(key, value);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getProperty(String key) {
-        return (T)properties.get(key);
+        return (T) properties.get(key);
     }
 
     public Map<String, Object> getProperties() {
@@ -29,16 +42,10 @@ public class FeatureCollectionWithProperties extends FeatureCollection {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        if (!super.equals(o)) {
-            return false;
-        }
-        FeatureCollectionWithProperties fc = (FeatureCollectionWithProperties)o;
-        if (!Objects.equals(properties, fc.properties))
-            return false;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FeatureCollectionWithProperties fc = (FeatureCollectionWithProperties) o;
+        if (!Objects.equals(properties, fc.properties)) return false;
         return this.getFeatures().equals(fc.getFeatures());
     }
 
@@ -51,6 +58,10 @@ public class FeatureCollectionWithProperties extends FeatureCollection {
 
     @Override
     public String toString() {
-        return "FeatureCollection{properties=" + properties + ", features=" + this.getFeatures() + '}';
+        return "FeatureCollection{properties="
+            + properties
+            + ", features="
+            + this.getFeatures()
+            + '}';
     }
 }
