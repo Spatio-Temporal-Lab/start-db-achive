dbb4dea79ee66638bc3e05ff9074881d09170ffe
jimolonely <wp147188@foxmail.com>
2022-06-06 07:35:18 +0800

    add user auth

---
+++ start-db-server/src/main/java/org/urbcomp/start/db/server/AuthenticationHelper.java
@@ -0,0 +1,12 @@
+package org.urbcomp.start.db.server;
+
+/**
+ * @author jimo
+ **/
+public class AuthenticationHelper {
+
+    public static boolean auth(String username, String password) {
+        // TODO
+        return false;
+    }
+}
