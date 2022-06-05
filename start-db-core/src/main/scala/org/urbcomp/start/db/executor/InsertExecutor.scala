14ab616bbdad5243cd764ba4e59edf58af056f9c
Junwen Liu <86091013+zaiyuan-ljw@users.noreply.github.com>
2022-06-05 22:03:46 +0800

    add insert executor (#113)
    Co-authored-by: zaiyuan49 <zhugeliang0511>

--- start-db-core/src/main/scala/org/urbcomp/start/db/executor/InsertExecutor.scala
+++ start-db-core/src/main/scala/org/urbcomp/start/db/executor/InsertExecutor.scala
@@ -11,11 +11,91 @@
 
 package org.urbcomp.start.db.executor
 
-import org.apache.calcite.sql.SqlInsert
+import org.apache.calcite.sql.{SqlBasicCall, SqlIdentifier, SqlInsert}
+import org.geotools.data.{DataStoreFinder, Transaction}
+import org.locationtech.geomesa.utils.io.WithClose
 import org.urbcomp.start.db.infra.BaseExecutor
 
+import java.sql.{DriverManager, ResultSet}
+import java.util
+import java.util.Properties
+
+/**
+  * @author zaiyuan
+  * @param n
+  */
 case class InsertExecutor(n: SqlInsert) extends BaseExecutor {
 
-  override def execute(): Unit = ??? // TODO
+  override def execute(): Unit = {
+    // extract database name and table name
+    val userName = ""
+    val envDbName = ""
+    val targetTable = n.getTargetTable.asInstanceOf[SqlIdentifier]
+    val (dbName, tableName) = targetTable.names.size() match {
+      case 2 =>
+        (targetTable.names.get(2), targetTable.names.get(2))
+      case 1 =>
+        (envDbName, targetTable.names.get(2))
+      case _ =>
+        throw new RuntimeException("target table format should like dbname.tablename or tablename")
+    }
+
+    // TODO validate metadata
+
+    // construct sql
+    val resultObjs: Array[util.ArrayList[AnyRef]] =
+      n.getSource
+        .asInstanceOf[SqlBasicCall] // Values level
+        .operands
+        .map { i => // Row level
+          val queryItem = i
+            .asInstanceOf[SqlBasicCall]
+            .operands
+            .map(j => j.toString) // TODO nee test
+            .mkString(" , ")
+          val querySql =
+            s"""
+             |SELECT $queryItem
+             |""".stripMargin
+          val rs = executeQuery(userName, dbName, querySql)
+          val count = rs.getMetaData.getColumnCount
+          val result = new util.ArrayList[AnyRef](count)
+          for (x <- 1 until count) {
+            result.add(rs.getObject(x))
+          }
+          result
+        }
+
+    // insert data
+    val dataStore = DataStoreFinder.getDataStore(new util.HashMap[String, String]())
+    WithClose(dataStore.getFeatureWriterAppend(tableName, Transaction.AUTO_COMMIT)) { writer =>
+      resultObjs.foreach { i =>
+        val sf = writer.next()
+        val count = i.size()
+        for (x <- 1 until count) {
+          sf.setAttribute(n.getTargetColumnList.get(x).toString, i.get(x))
+        }
+        writer.write()
+      }
+    }
+    dataStore.dispose()
+  }
+
+  def executeQuery[R](user: String, dbName: String, querySql: String): ResultSet = {
+    System.setProperty("jdbc.drivers", "org.apache.calcite.jdbc.Driver")
+    val driver = Class.forName("org.apache.calcite.jdbc.Driver")
+    assert(driver != null)
+    val properties = new Properties()
+    properties.setProperty("user", user)
+    properties.setProperty("db", dbName)
+    properties.setProperty("printLog", "false")
+    properties.setProperty("recordHistory", "false")
+
+    WithClose(DriverManager.getConnection("jdbc:calcite:fun=spatial", properties)) { conn =>
+      WithClose(conn.createStatement()) { stmt =>
+        stmt.executeQuery(querySql)
+      }
+    }
+  }
 
 }
