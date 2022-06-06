793afac41160bdad725ed49f611a6c2e5165bfcc
jimolonely <wp147188@foxmail.com>
2022-06-06 19:17:42 +0800

    add user auth

--- start-db-core/src/main/resources/metadata/ddl.sql
+++ start-db-core/src/main/resources/metadata/ddl.sql
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `sys_field`
   DEFAULT CHARSET = utf8;
 
 
--- 插入测试用户、库、表，DDL好了后就没必要手动插入了
+-- 插入测试用户、库、表，DDL好了后就没必要手动插入了, 密码是 start-db
 INSERT INTO sys_user(id, name, password) VALUE (1, 'start_db', '6ee5f4a8b6a2697f5add5ab1333968d0');
 INSERT INTO sys_database(id, user_id, name) VALUE (1, 1, 'db_test');
 INSERT INTO sys_table(id, db_id, name, storage_engine) VALUE (1, 1, 't_test', 'hbase');
