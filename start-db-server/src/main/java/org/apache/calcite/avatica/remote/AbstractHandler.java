0ff3fabe2c7c04873e1183bd3e3fd1b9d1239f18
jimolonely <wp147188@foxmail.com>
2022-06-06 22:11:35 +0800

    fix format

--- start-db-server/src/main/java/org/apache/calcite/avatica/remote/AbstractHandler.java
+++ start-db-server/src/main/java/org/apache/calcite/avatica/remote/AbstractHandler.java
@@ -1,19 +1,14 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to you under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2022 ST-Lab
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  */
+
 package org.apache.calcite.avatica.remote;
 
 import org.apache.calcite.avatica.AvaticaSeverity;
@@ -94,9 +89,11 @@ public abstract class AbstractHandler<T> implements Handler<T> {
         try {
             final Service.Request request = decode(serializedRequest);
             if (request instanceof Service.OpenConnectionRequest) {
-                Service.OpenConnectionRequest openConnection = (Service.OpenConnectionRequest) request;
+                Service.OpenConnectionRequest openConnection =
+                    (Service.OpenConnectionRequest) request;
                 final Map<String, String> info = openConnection.info;
-                if (info == null || !AuthenticationHelper.auth(info.get("user"), info.get("password"))) {
+                if (info == null
+                    || !AuthenticationHelper.auth(info.get("user"), info.get("password"))) {
                     // TODO custom exception
                     throw new RuntimeException("Auth Failed");
                 }
