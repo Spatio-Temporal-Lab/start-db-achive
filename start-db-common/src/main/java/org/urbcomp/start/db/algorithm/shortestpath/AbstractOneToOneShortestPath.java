9e4c8cbb8ac94b53ab818fa372c23cb427d6742f
Leo <liruiyuanwhu@qq.com>
2022-06-08 22:46:39 +0800

    roadnetwork and roadsegment class add r-tree etc.

--- start-db-common/src/main/java/org/urbcomp/start/db/algorithm/shortestpath/AbstractOneToOneShortestPath.java
+++ start-db-common/src/main/java/org/urbcomp/start/db/algorithm/shortestpath/AbstractOneToOneShortestPath.java
@@ -11,13 +11,33 @@
 
 package org.urbcomp.start.db.algorithm.shortestpath;
 
+import org.urbcomp.start.db.model.point.CandidatePoint;
+import org.urbcomp.start.db.model.point.SpatialPoint;
 import org.urbcomp.start.db.model.roadsegment.RoadNetwork;
+import org.urbcomp.start.db.model.roadsegment.RoadNode;
+import org.urbcomp.start.db.model.roadsegment.RoadSegment;
 
-public abstract class AbstractOneToOneShortestPath implements IShortestPath {
+public abstract class AbstractOneToOneShortestPath {
     private RoadNetwork roadNetwork;
     /**
      * note that the start and end points may be mot exactly on the road segments,
      * the searchDistance defines the scope to find the start or end road segments.
+     * unit: meter
      */
     private double searchDistance = 300;
+
+    public AbstractOneToOneShortestPath(RoadNetwork roadNetwork, double searchDistance) {
+        this.roadNetwork = roadNetwork;
+        this.searchDistance = searchDistance;
+    }
+
+    public AbstractOneToOneShortestPath(RoadNetwork roadNetwork) {
+        this.roadNetwork = roadNetwork;
+    }
+
+    protected abstract ShortestPathResult findShortestPath(RoadNode startNode, RoadNode endNode);
+
+    public ShortestPathResult findShortestPath(SpatialPoint startPoint, SpatialPoint endPoint) {
+
+    }
 }
