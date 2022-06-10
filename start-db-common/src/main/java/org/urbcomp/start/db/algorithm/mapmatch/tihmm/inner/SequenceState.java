dc3451ead4e5bc80bb3bfe2e452de01674af9067
Jun Jiang <liruiyuanwhu@qq.com>
2022-06-10 11:45:51 +0800

    添加TiHMM地图匹配算法

---
+++ start-db-common/src/main/java/org/urbcomp/start/db/algorithm/mapmatch/tihmm/inner/SequenceState.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2022 ST-Lab
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
+package org.urbcomp.start.db.algorithm.mapmatch.tihmm.inner;
+
+import org.urbcomp.start.db.model.point.CandidatePoint;
+import org.urbcomp.start.db.model.point.GPSPoint;
+
+/**
+ * 辅助class，保存状态信息
+ *
+ * @author : Haowen Zhu
+ * @date : 2019/09/027
+ */
+public class SequenceState {
+    /**
+     * 原始point的candidate
+     */
+    private final CandidatePoint state;
+    /**
+     * 原始point
+     */
+    private final GPSPoint observation;
+
+    /**
+     * 构造函数
+     * @param state 原始point的candidate
+     * @param observation 原始point
+     */
+    public SequenceState(CandidatePoint state, GPSPoint observation) {
+        this.state = state;
+        this.observation = observation;
+    }
+
+    public CandidatePoint getState() {
+        return state;
+    }
+
+    public GPSPoint getObservation() {
+        return observation;
+    }
+
+}
