64518c76c85dce63e09199e05a6bc981cdf9538e
Jun Jiang <liruiyuanwhu@qq.com>
2022-06-10 11:45:51 +0800

    添加TiHMM地图匹配算法

---
+++ start-db-common/src/main/java/org/urbcomp/start/db/algorithm/mapmatch/tihmm/TiHmmMapMatcher.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright 2022 ST-Lab
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
+package org.urbcomp.start.db.algorithm.mapmatch.tihmm;
+
+import org.urbcomp.start.db.algorithm.mapmatch.tihmm.inner.HmmProbabilities;
+import org.urbcomp.start.db.algorithm.mapmatch.tihmm.inner.SequenceState;
+import org.urbcomp.start.db.algorithm.mapmatch.tihmm.inner.TiViterbi;
+import org.urbcomp.start.db.algorithm.mapmatch.tihmm.inner.TimeStep;
+import org.urbcomp.start.db.algorithm.shortestpath.AbstractShortestPath;
+import org.urbcomp.start.db.algorithm.shortestpath.BiDijkstraShortestPath;
+import org.urbcomp.start.db.exception.AlgorithmExecuteException;
+import org.urbcomp.start.db.model.point.CandidatePoint;
+import org.urbcomp.start.db.model.point.GPSPoint;
+import org.urbcomp.start.db.model.point.MapMatchedPoint;
+import org.urbcomp.start.db.model.roadnetwork.Path;
+import org.urbcomp.start.db.model.roadnetwork.RoadNetwork;
+import org.urbcomp.start.db.model.trajectory.MapMatchedTrajectory;
+import org.urbcomp.start.db.model.trajectory.Trajectory;
+import org.urbcomp.start.db.util.GeoFunctions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TiHmmMapMatcher {
+    /**
+     * emission P用正态分布函数来模拟，sigma为正态分布的概率函数参数
+     */
+    private static final double measurementErrorSigma = 50.0;
+    /**
+     * transition p 的指数概率函数参数
+     */
+    private static final double transitionProbabilityBeta = 2.0;
+
+    /**
+     * 路网
+     */
+    protected final RoadNetwork roadNetwork;
+
+    protected final AbstractShortestPath pathAlgo;
+
+    public TiHmmMapMatcher(RoadNetwork roadNetwork) {
+        this.roadNetwork = roadNetwork;
+        this.pathAlgo = new BiDijkstraShortestPath(roadNetwork);
+    }
+
+    /**
+     * 实现抽象类的map match 方法
+     *
+     * @param traj 原始轨迹
+     * @return map match后的轨迹
+     */
+    public MapMatchedTrajectory matchTrajToMapMatchedTraj(Trajectory traj)
+        throws AlgorithmExecuteException {
+        List<SequenceState> seq = this.computeViterbiSequence(traj.getGPSPointList());
+        assert traj.getGPSPointList().size() == seq.size();
+        List<MapMatchedPoint> mapMatchedPointList = new ArrayList<>(seq.size());
+        for (SequenceState ss : seq) {
+            CandidatePoint candiPt = null;
+            if (ss.getState() != null) {
+                candiPt = ss.getState();
+            }
+            mapMatchedPointList.add(new MapMatchedPoint(ss.getObservation(), candiPt));
+        }
+        return new MapMatchedTrajectory(traj.getOid(), mapMatchedPointList);
+    }
+
+    /**
+     * 建立一个time step
+     *
+     * @param pt 原始轨迹点
+     * @return timestep
+     */
+    private TimeStep createTimeStep(GPSPoint pt) {
+        TimeStep timeStep = null;
+        List<CandidatePoint> candidates = CandidatePoint.getCandidatePoint(
+            pt,
+            roadNetwork,
+            measurementErrorSigma
+        );
+        if (!candidates.isEmpty()) {
+            timeStep = new TimeStep(pt, candidates);
+        }
+        return timeStep;
+    }
+
+    /**
+     * 计算一个 Viterbi sequence
+     *
+     * @param ptList 原始轨迹ptList
+     * @return 保存了每一步step的所有状态
+     */
+    private List<SequenceState> computeViterbiSequence(List<GPSPoint> ptList)
+        throws AlgorithmExecuteException {
+        List<SequenceState> seq = new ArrayList<>();
+        final HmmProbabilities probabilities = new HmmProbabilities(
+            measurementErrorSigma,
+            transitionProbabilityBeta
+        );
+        TiViterbi viterbi = new TiViterbi();
+        TimeStep preTimeStep = null;
+        int idx = 0;
+        int nbPoints = ptList.size();
+        while (idx < nbPoints) {
+            TimeStep timeStep = this.createTimeStep(ptList.get(idx));
+            if (timeStep == null) {
+                seq.addAll(viterbi.computeMostLikelySequence());
+                seq.add(new SequenceState(null, ptList.get(idx)));
+                viterbi = new TiViterbi();
+                preTimeStep = null;
+            } else {
+                this.computeEmissionProbabilities(timeStep, probabilities);
+                if (preTimeStep == null) {
+                    viterbi.startWithInitialObservation(
+                        timeStep.getObservation(),
+                        timeStep.getCandidates(),
+                        timeStep.getEmissionLogProbabilities()
+                    );
+                } else {
+                    this.computeTransitionProbabilities(preTimeStep, timeStep, probabilities);
+                    viterbi.nextStep(
+                        timeStep.getObservation(),
+                        timeStep.getCandidates(),
+                        timeStep.getEmissionLogProbabilities(),
+                        timeStep.getTransitionLogProbabilities()
+                    );
+                }
+                if (viterbi.isBroken) {
+                    seq.addAll(viterbi.computeMostLikelySequence());
+                    viterbi = new TiViterbi();
+                    viterbi.startWithInitialObservation(
+                        timeStep.getObservation(),
+                        timeStep.getCandidates(),
+                        timeStep.getEmissionLogProbabilities()
+                    );
+                }
+                preTimeStep = timeStep;
+            }
+            idx += 1;
+        }
+        if (seq.size() < nbPoints) {
+            seq.addAll(viterbi.computeMostLikelySequence());
+        }
+        return seq;
+    }
+
+    /**
+     * 根据time step和概率分布函数计算emission P
+     *
+     * @param timeStep    timeStep
+     * @param probability 建立好的概率分布函数
+     */
+    private void computeEmissionProbabilities(TimeStep timeStep, HmmProbabilities probability) {
+        for (CandidatePoint candiPt : timeStep.getCandidates()) {
+            final double dist = candiPt.getErrorDistanceInMeter();
+            timeStep.addEmissionLogProbability(candiPt, probability.emissionLogProbability(dist));
+        }
+    }
+
+    /**
+     * 计算之前timeStep到当前timeStep的概率
+     *
+     * @param prevTimeStep  之前的timestep
+     * @param timeStep      当前的timestep
+     * @param probabilities 建立好的概率分布函数
+     */
+    protected void computeTransitionProbabilities(
+        TimeStep prevTimeStep,
+        TimeStep timeStep,
+        HmmProbabilities probabilities
+    ) throws AlgorithmExecuteException {
+        final double linearDist = GeoFunctions.getDistanceInM(
+            prevTimeStep.getObservation(),
+            timeStep.getObservation()
+        );
+        for (CandidatePoint preCandiPt : prevTimeStep.getCandidates()) {
+            for (CandidatePoint curCandiPt : timeStep.getCandidates()) {
+                Path path = pathAlgo.findShortestPath(preCandiPt, curCandiPt);
+                if (path.getLengthInMeter() != Double.MAX_VALUE) {
+                    timeStep.addTransitionLogProbability(
+                        preCandiPt,
+                        curCandiPt,
+                        probabilities.transitionLogProbability(path.getLengthInMeter(), linearDist)
+                    );
+                }
+            }
+        }
+    }
+}
---
+++ start-db-common/src/main/java/org/urbcomp/start/db/algorithm/mapmatch/tihmm/inner/TiViterbi.java
@@ -0,0 +1,265 @@
+/*
+ * Copyright 2022 ST-Lab
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
+package org.urbcomp.start.db.algorithm.mapmatch.tihmm.inner;
+
+import org.urbcomp.start.db.model.point.CandidatePoint;
+import org.urbcomp.start.db.model.point.GPSPoint;
+import scala.Tuple2;
+
+import java.util.*;
+
+/**
+ * Tihmm 核心算法class
+ */
+public class TiViterbi {
+    /**
+     * 上一个状态下每一个candidate point 对应的extended state
+     */
+    private Map<CandidatePoint, ExtendedState> lastExtendedStates;
+    /**
+     * 上一个状态下原始轨迹点对应的所有candidate point
+     */
+    private List<CandidatePoint> prevCandidates;
+    /**
+     * 每个candidate point 对应的p
+     */
+    public Map<CandidatePoint, Double> message;
+    /**
+     * 是否停止初始化状态概率函数
+     */
+    public Boolean isBroken = false;
+
+    /**
+     * 初始状态概率函数
+     *
+     * @param observation             原始轨迹点
+     * @param candidates              原始轨迹点对应的candidates
+     * @param initialLogProbabilities 初始状态概率
+     */
+    private void initializeStateProbabilities(
+        GPSPoint observation,
+        List<CandidatePoint> candidates,
+        Map<CandidatePoint, Double> initialLogProbabilities
+    ) {
+        if (message != null) {
+            throw new IllegalArgumentException("Initial probabilities have already been set.");
+        }
+        Map<CandidatePoint, Double> initialMessage = new HashMap<>(initialLogProbabilities.size());
+        for (CandidatePoint candidate : candidates) {
+            if (!initialLogProbabilities.containsKey(candidate)) {
+                throw new IllegalArgumentException("No initial probability for " + candidate);
+            }
+            double logProbability = initialLogProbabilities.get(candidate);
+            initialMessage.put(candidate, logProbability);
+        }
+        isBroken = hmmBreak(initialMessage);
+        if (isBroken) {
+            return;
+        }
+        message = initialMessage;
+        lastExtendedStates = new HashMap<>(candidates.size());
+        prevCandidates = new ArrayList<>(candidates.size());
+        for (CandidatePoint candidate : candidates) {
+            lastExtendedStates.put(candidate, new ExtendedState(candidate, null, observation));
+            prevCandidates.add(candidate);
+        }
+    }
+
+    /**
+     * 如果初始化的概率有接近武无穷小的，停止初始化
+     *
+     * @param message 状态概率
+     */
+    private Boolean hmmBreak(Map<CandidatePoint, Double> message) {
+        for (Double logProbability : message.values()) {
+            if (!logProbability.equals(Double.NEGATIVE_INFINITY)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 开始viterbi计算，向前extend
+     *
+     * @param observation                原始轨迹点
+     * @param prevCandidates             之前的candidates
+     * @param curCandidates              当前的candidates
+     * @param message                    状态概率
+     * @param emissionLogProbabilities   emission p
+     * @param transitionLogProbabilities transition p
+     * @return 向前extend后的结果
+     */
+    private ForwardStepResult forwardStep(
+        GPSPoint observation,
+        List<CandidatePoint> prevCandidates,
+        List<CandidatePoint> curCandidates,
+        Map<CandidatePoint, Double> message,
+        Map<CandidatePoint, Double> emissionLogProbabilities,
+        Map<Tuple2<CandidatePoint, CandidatePoint>, Double> transitionLogProbabilities
+    ) {
+        final ForwardStepResult result = new ForwardStepResult(curCandidates.size());
+        assert prevCandidates.size() != 0;
+        for (CandidatePoint curState : curCandidates) {
+            double maxLogProbability = Double.NEGATIVE_INFINITY;
+            CandidatePoint maxPreState = null;
+            for (CandidatePoint preState : prevCandidates) {
+                final double logProbability = message.get(preState) + transitionLogProbability(
+                    preState,
+                    curState,
+                    transitionLogProbabilities
+                );
+                if (logProbability > maxLogProbability) {
+                    maxLogProbability = logProbability;
+                    maxPreState = preState;
+                }
+            }
+            result.getNewMessage()
+                .put(curState, (maxLogProbability + emissionLogProbabilities.get(curState)));
+            // Note that max_prev_state == None if there is no transition with non-zero probability.
+            // In this case cur_state has zero probability and will not be part of the most likely
+            // sequence,
+            // so we don't need an ExtendedState.
+            if (maxPreState != null) {
+                final ExtendedState extendedState = new ExtendedState(
+                    curState,
+                    lastExtendedStates.get(maxPreState),
+                    observation
+                );
+                result.getNewExtendedStates().put(curState, extendedState);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 给定transition 计算对应概率
+     *
+     * @param preState                   之前的state
+     * @param curState                   现在的state
+     * @param transitionLogProbabilities 转移概率的map
+     * @return 概率p
+     */
+    private double transitionLogProbability(
+        CandidatePoint preState,
+        CandidatePoint curState,
+        Map<Tuple2<CandidatePoint, CandidatePoint>, Double> transitionLogProbabilities
+    ) {
+        Tuple2<CandidatePoint, CandidatePoint> transition = new Tuple2<>(preState, curState);
+        return transitionLogProbabilities.getOrDefault(transition, Double.NEGATIVE_INFINITY);
+    }
+
+    /**
+     * 计算当前状态下概率最大对应的state
+     *
+     * @return candidate point
+     */
+    private CandidatePoint mostLikelyState() {
+        assert message.size() != 0;
+        CandidatePoint result = null;
+        Double maxLogProbability = Double.NEGATIVE_INFINITY;
+        for (Map.Entry<CandidatePoint, Double> entry : message.entrySet()) {
+            if (entry.getValue() > maxLogProbability) {
+                result = entry.getKey();
+                maxLogProbability = entry.getValue();
+            }
+        }
+        assert result != null;
+        return result;
+    }
+
+    /**
+     * 取回概率最大的一系列转移过程
+     *
+     * @return list，包含每一步转移对应的状态
+     */
+    private List<SequenceState> retrieveMostLikelySequence() {
+        assert message.size() != 0;
+        CandidatePoint lastState = mostLikelyState();
+        List<SequenceState> result = new ArrayList<>();
+        ExtendedState es = lastExtendedStates.get(lastState);
+        while (es != null) {
+            SequenceState ss = new SequenceState(es.getState(), es.getObservation());
+            result.add(ss);
+            es = es.getBackPointer();
+        }
+        Collections.reverse(result);
+        return result;
+    }
+
+    /**
+     * 初始化启动第一步viterbi计算
+     *
+     * @param observation              原始轨迹点
+     * @param candidates               对应的candidates
+     * @param emissionLogProbabilities 每一个candidate对应的 emission p
+     */
+    public void startWithInitialObservation(
+        GPSPoint observation,
+        List<CandidatePoint> candidates,
+        Map<CandidatePoint, Double> emissionLogProbabilities
+    ) {
+        initializeStateProbabilities(observation, candidates, emissionLogProbabilities);
+    }
+
+    /**
+     * 向下一步viterbi计算， 更新当前状态的信息
+     *
+     * @param observation                原始轨迹点
+     * @param candidates                 对应的candidates
+     * @param emissionLogProbabilities   每一个candidate对应的 emission p
+     * @param transitionLogProbabilities 每一个transition对应的 transition p
+     */
+    public void nextStep(
+        GPSPoint observation,
+        List<CandidatePoint> candidates,
+        Map<CandidatePoint, Double> emissionLogProbabilities,
+        Map<Tuple2<CandidatePoint, CandidatePoint>, Double> transitionLogProbabilities
+    ) {
+        if (message == null) {
+            throw new IllegalStateException(
+                "start with initial observation() must be called first."
+            );
+        }
+        if (isBroken) {
+            throw new IllegalStateException("Method must not be called after an HMM break.");
+        }
+        ForwardStepResult forwardStepResult = forwardStep(
+            observation,
+            prevCandidates,
+            candidates,
+            message,
+            emissionLogProbabilities,
+            transitionLogProbabilities
+        );
+        isBroken = hmmBreak(forwardStepResult.getNewMessage());
+        if (isBroken) {
+            return;
+        }
+        message = forwardStepResult.getNewMessage();
+        lastExtendedStates = forwardStepResult.getNewExtendedStates();
+        prevCandidates = new ArrayList<>(candidates);
+    }
+
+    /**
+     * 辅助函数，下概率最大对应的每一步的extended state 组成的list
+     *
+     * @return list 包含每一步对应的extended state
+     */
+    public List<SequenceState> computeMostLikelySequence() {
+        if (message == null) {
+            return new ArrayList<>();
+        } else {
+            return retrieveMostLikelySequence();
+        }
+    }
+}
