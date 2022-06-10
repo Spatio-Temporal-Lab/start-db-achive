8f5a972822f606c11f2c80537a6f39606f45e1f6
jimolonely <wp147188@foxmail.com>
2022-06-10 21:19:40 +0800

    fix format

--- start-db-calcite/src/main/java/org/urbcomp/start/db/infra/MetadataResult.java
+++ start-db-calcite/src/main/java/org/urbcomp/start/db/infra/MetadataResult.java
@@ -1,3 +1,25 @@
+/*
+ * This file is inherited from Apache Calcite and modifed by ST-Lab under apache license.
+ * You can find the original code from
+ *
+ * https://github.com/apache/calcite
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.urbcomp.start.db.infra;
 
 import com.google.common.collect.ImmutableList;
@@ -27,8 +49,32 @@ import java.util.Map;
  **/
 public class MetadataResult<T> extends CalcitePrepare.CalciteSignature<T> {
 
-    private MetadataResult(String sql, List<AvaticaParameter> parameterList, Map<String, Object> internalParameters, RelDataType rowType, List<ColumnMetaData> columns, Meta.CursorFactory cursorFactory, CalciteSchema rootSchema, List<RelCollation> collationList, long maxRowCount, Bindable<T> bindable, Meta.StatementType statementType) {
-        super(sql, parameterList, internalParameters, rowType, columns, cursorFactory, rootSchema, collationList, maxRowCount, bindable, statementType);
+    private MetadataResult(
+        String sql,
+        List<AvaticaParameter> parameterList,
+        Map<String, Object> internalParameters,
+        RelDataType rowType,
+        List<ColumnMetaData> columns,
+        Meta.CursorFactory cursorFactory,
+        CalciteSchema rootSchema,
+        List<RelCollation> collationList,
+        long maxRowCount,
+        Bindable<T> bindable,
+        Meta.StatementType statementType
+    ) {
+        super(
+            sql,
+            parameterList,
+            internalParameters,
+            rowType,
+            columns,
+            cursorFactory,
+            rootSchema,
+            collationList,
+            maxRowCount,
+            bindable,
+            statementType
+        );
     }
 
     /**
@@ -39,22 +85,33 @@ public class MetadataResult<T> extends CalcitePrepare.CalciteSignature<T> {
     }
 
     public MetadataResult(List<ColumnMetaData> metaData, Bindable<T> bindable) {
-        this("",
-                ImmutableList.of(),
-                ImmutableMap.of(),
-                null,
-                metaData,
-                Meta.CursorFactory.OBJECT,
-                null,
-                ImmutableList.of(),
-                -1,
-                bindable,
-                Meta.StatementType.OTHER_DDL);
+        this(
+            "",
+            ImmutableList.of(),
+            ImmutableMap.of(),
+            null,
+            metaData,
+            Meta.CursorFactory.OBJECT,
+            null,
+            ImmutableList.of(),
+            -1,
+            bindable,
+            Meta.StatementType.OTHER_DDL
+        );
     }
 
     public static <T> MetadataResult<T> buildDDLResult(int affectRows) {
         List<ColumnMetaData> metaData = new ArrayList<>(1);
-        metaData.add(ColumnMetaData.dummy(new ColumnMetaData.ScalarType(SqlType.INTEGER.id, "affectRows", ColumnMetaData.Rep.INTEGER), true));
+        metaData.add(
+            ColumnMetaData.dummy(
+                new ColumnMetaData.ScalarType(
+                    SqlType.INTEGER.id,
+                    "affectRows",
+                    ColumnMetaData.Rep.INTEGER
+                ),
+                true
+            )
+        );
         return new MetadataResult<>(metaData);
     }
 
@@ -64,7 +121,16 @@ public class MetadataResult<T> extends CalcitePrepare.CalciteSignature<T> {
     public static MetadataResult<Object[]> buildResult(String[] columns, List<Object[]> values) {
         List<ColumnMetaData> metaData = new ArrayList<>(1);
         for (String column : columns) {
-            metaData.add(ColumnMetaData.dummy(new ColumnMetaData.ScalarType(SqlType.VARCHAR.id, column, ColumnMetaData.Rep.STRING), true));
+            metaData.add(
+                ColumnMetaData.dummy(
+                    new ColumnMetaData.ScalarType(
+                        SqlType.VARCHAR.id,
+                        column,
+                        ColumnMetaData.Rep.STRING
+                    ),
+                    true
+                )
+            );
         }
         return new MetadataResult<>(metaData, new MetaBindable(values));
     }
@@ -106,8 +172,7 @@ public class MetadataResult<T> extends CalcitePrepare.CalciteSignature<T> {
                         }
 
                         @Override
-                        public void close() {
-                        }
+                        public void close() {}
                     };
                 }
             };
