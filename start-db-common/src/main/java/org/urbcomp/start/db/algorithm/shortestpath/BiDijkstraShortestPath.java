fcb74aee8feef1fded859ac2e176ad7135875bf5
Leo <liruiyuanwhu@qq.com>
2022-06-10 09:21:00 +0800

    refactor

--- start-db-common/src/main/java/org/urbcomp/start/db/algorithm/shortestpath/BiDijkstraShortestPath.java
+++ start-db-common/src/main/java/org/urbcomp/start/db/algorithm/shortestpath/BiDijkstraShortestPath.java
@@ -11,43 +11,15 @@
 
 package org.urbcomp.start.db.algorithm.shortestpath;
 
-import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.BidirectionalDijkstraShortestPath;
-import org.urbcomp.start.db.model.point.SpatialPoint;
-import org.urbcomp.start.db.model.roadsegment.Path;
 import org.urbcomp.start.db.model.roadsegment.RoadNetwork;
-import org.urbcomp.start.db.model.roadsegment.RoadNode;
-import org.urbcomp.start.db.model.roadsegment.RoadSegment;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class BiDijkstraShortestPath extends AbstractShortestPath {
     public BiDijkstraShortestPath(RoadNetwork roadNetwork, double searchDistance) {
-        super(roadNetwork, searchDistance);
+        super(roadNetwork, searchDistance, new BidirectionalDijkstraShortestPath<>(roadNetwork.getDirectedRoadGraph()));
     }
 
     public BiDijkstraShortestPath(RoadNetwork roadNetwork) {
-        super(roadNetwork);
-    }
-
-    @Override
-    protected Path findShortestPath(RoadNode startNode, RoadNode endNode) {
-        BidirectionalDijkstraShortestPath<RoadNode, RoadSegment> algo =
-            new BidirectionalDijkstraShortestPath<>(roadNetwork.getDirectedRoadGraph());
-        GraphPath<RoadNode, RoadSegment> shortestPath = algo.getPath(startNode, endNode);
-        if (shortestPath == null) {
-            return new Path(Double.MAX_VALUE, new ArrayList<>(), new ArrayList<>());
-        } else {
-            List<SpatialPoint> points = new ArrayList<>();
-            double length = 0;
-            List<Integer> roadSegmentIds = new ArrayList<>();
-            for (RoadSegment rs : shortestPath.getEdgeList()) {
-                length += rs.getLengthInMeter();
-                points.addAll(rs.getPoints());
-                roadSegmentIds.add(rs.getRoadSegmentId());
-            }
-            return new Path(length, points, roadSegmentIds);
-        }
+        super(roadNetwork, new BidirectionalDijkstraShortestPath<>(roadNetwork.getDirectedRoadGraph()));
     }
 }
