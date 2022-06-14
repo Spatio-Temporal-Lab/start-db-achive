273edb56e6b5fbad968c6916925a61f6298d2f10
cquhx <48610039+cquhx@users.noreply.github.com>
2022-06-14 16:49:34 +0800

    add road segment io

--- start-db-core/src/main/java/org/urbcomp/start/db/io/RoadSegmentGeoMesaIO.java
+++ start-db-core/src/main/java/org/urbcomp/start/db/io/RoadSegmentGeoMesaIO.java
@@ -12,14 +12,20 @@
 package org.urbcomp.start.db.io;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.geotools.data.DataStore;
-import org.geotools.data.DataStoreFinder;
-import org.geotools.data.FeatureWriter;
-import org.geotools.data.Transaction;
+import org.geotools.data.*;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.filter.identity.FeatureIdImpl;
+import org.geotools.filter.text.cql2.CQLException;
+import org.geotools.filter.text.ecql.ECQL;
 import org.geotools.util.factory.Hints;
+import org.jaitools.imageutils.iterator.SimpleIterator;
 import org.locationtech.geomesa.utils.interop.SimpleFeatureTypes;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.urbcomp.start.db.model.roadnetwork.RoadSegment;
@@ -38,10 +44,10 @@ public class RoadSegmentGeoMesaIO {
      */
     public SimpleFeatureType getSimpleFeatureType() {
         SimpleFeatureType sft = SimpleFeatureTypes.createType(
-            "RoadSegment",
-            "rsId:int,"
-                + ""
-                + "geoJson:String"
+                "RoadSegment",
+                "rsId:int,"
+                        + "*geom:LineString:srid=4326,"
+                        + "geoJson:String"
         );
         sft.getUserData().put(SimpleFeatureTypes.DEFAULT_DATE_KEY, "dtg");
         return sft;
@@ -51,12 +57,13 @@ public class RoadSegmentGeoMesaIO {
         try {
             System.out.println("write test data");
             FeatureWriter<SimpleFeatureType, SimpleFeature> writer = dataStore
-                .getFeatureWriterAppend(sft.getTypeName(), Transaction.AUTO_COMMIT);
+                    .getFeatureWriterAppend(sft.getTypeName(), Transaction.AUTO_COMMIT);
             SimpleFeature toWrite = writer.next();
             toWrite.setAttributes(feature.getAttributes());
             ((FeatureIdImpl) toWrite.getIdentifier()).setID(feature.getID());
             toWrite.getUserData().put(Hints.USE_PROVIDED_FID, Boolean.TRUE);
             toWrite.getUserData().putAll(feature.getUserData());
+//            System.out.println(sft);
             writer.write();
             // 关闭流
             writer.close();
@@ -66,15 +73,12 @@ public class RoadSegmentGeoMesaIO {
 
     }
 
-    public SimpleFeature getData(RoadSegment rs, SimpleFeatureType sft) throws JsonProcessingException, JsonProcessingException {
+    public SimpleFeature getData(RoadSegment rs, SimpleFeatureType sft) throws JsonProcessingException {
         SimpleFeatureBuilder builder = new SimpleFeatureBuilder(sft);
         builder.set("rsId", rs.getRoadSegmentId());
-        builder.set("direction", rs.getDirection().value());
-        builder.set("speedLimit", rs.getSpeedLimit());
-        builder.set("level", rs.getLevel().value());
-        builder.set("startId", rs.getStartNode().getNodeId());
-        builder.set("endId", rs.getEndNode().getNodeId());
-        builder.set("lengthInMeter", rs.getLengthInMeter());
+        builder.set("geom", new GeometryFactory().createLineString(
+                rs.getPoints().stream().map(Point::getCoordinate).toArray(Coordinate[]::new)
+        ));
         builder.set("geoJson", rs.toGeoJSON());
         builder.featureUserData(Hints.USE_PROVIDED_FID, Boolean.TRUE);
 
@@ -92,8 +96,45 @@ public class RoadSegmentGeoMesaIO {
 
         SimpleFeatureType sft = getSimpleFeatureType();
         SimpleFeature sf = getData(rs, sft);
+        System.out.println(sf);
         dataStore.createSchema(sft);
 
         writeFeature(dataStore, sft, sf);
+    }
+
+    public void RoadSegmentFromGeoMesaObject() throws IOException, CQLException {
+
+        Map<String, String> params = new HashMap<>();
+        String CATALOG = "start_db.db_test";
+        params.put("hbase.catalog", CATALOG);
+        params.put("hbase.zookeepers", "localhost:2181");
+
+        //先定义查询语句
+        String bbox = "bbox (geom, -180.0, -90.0, 180.0, 90.0)";
+
+        //声明Query查询对象
+        Query query = new Query("RoadSegment", ECQL.toFilter(bbox));
+
+        DataStore dataStore = DataStoreFinder.getDataStore(params);
+
+        //获取读取器reader
+        SimpleFeatureSource featureSource = dataStore.getFeatureSource("RoadSegment");
+        SimpleFeatureCollection sfc = featureSource.getFeatures();
+        SimpleFeatureIterator iterator = sfc.features();
+
+        while (iterator.hasNext()) {
+            SimpleFeature sf = iterator.next();
+            System.out.println(sf);
+        }
+
+//        FeatureReader<SimpleFeatureType, SimpleFeature> reader =
+//                dataStore.getFeatureReader(query, Transaction.AUTO_COMMIT);
+//        System.out.println(reader.getFeatureType());
+//        while (reader.hasNext()) {
+//            SimpleFeature feature = reader.next();
+//            String locationWKT = feature.getAttribute("geom").toString();
+//            System.out.println(locationWKT);
+//        }
+
     }
 }
