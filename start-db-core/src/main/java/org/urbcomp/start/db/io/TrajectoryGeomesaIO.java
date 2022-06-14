a3fb17da28a2464b12ee5397b033c2a72ed36893
LI_Or <1519316637@qq.com>
2022-06-14 15:20:43 +0800

    GeometricRelationTypeConversionFunction

---
+++ start-db-core/src/main/java/org/urbcomp/start/db/io/TrajectoryGeomesaIO.java
@@ -0,0 +1,11 @@
+//package org.urbcomp.start.db.io;
+//
+//import org.locationtech.geomesa.utils.interop.SimpleFeatureTypes;
+//import org.opengis.feature.simple.SimpleFeatureType;
+//
+//public class TrajectoryGeomesaIO {
+//
+//    public SimpleFeatureType getSimpleFeatureType(){
+////        SimpleFeatureType sft = SimpleFeatureTypes.createType()
+////    }
+//}
