3455776263ec561a5e2e835c8a8f5b34c2b7491f
LI_Or <1519316637@qq.com>
2022-06-16 21:47:19 +0800

    TrajectoryFunction

--- start-db-calcite/src/main/java/org/urbcomp/start/db/function/TrajectoryFunction.java
+++ start-db-calcite/src/main/java/org/urbcomp/start/db/function/TrajectoryFunction.java
@@ -1,8 +1,29 @@
+/*
+ * This file is inherited from Apache Calcite and modifed by ST-Lab under apache license.
+ * You can find the original code from
+ *
+ * https://github.com/apache/calcite
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.urbcomp.start.db.function;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.locationtech.jts.geom.Point;
-import org.urbcomp.start.db.model.sample.ModelGenerator;
 import org.urbcomp.start.db.model.trajectory.Trajectory;
 
 import java.sql.Timestamp;
@@ -66,7 +87,7 @@ public class TrajectoryFunction {
 
     @StartDBFunction("st_traj_lengthInKM")
     public double st_traj_lengthInKM(Trajectory trajectory) {
-        return 0.0;
+        return trajectory.getLengthInKm();
     }
 
 }
