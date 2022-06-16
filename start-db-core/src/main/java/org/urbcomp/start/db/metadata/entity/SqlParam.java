4d3bb979023a44a885b057261bbb72fd9086d2ef
Bohong Wang <69339435+bee-corn@users.noreply.github.com>
2022-06-16 19:54:22 +0800

    feat sql params (#175)
    Co-authored-by: beecorn <bohong_wang@hotmail.com>

---
+++ start-db-core/src/main/java/org/urbcomp/start/db/metadata/entity/SqlParam.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 ST-Lab
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
+package org.urbcomp.start.db.metadata.entity;
+
+/**
+ * SqlParams: username, DBName
+ * @author Wang Bohong
+ * @date    2022-06-14
+ */
+public class SqlParam {
+
+    /**
+     * user name
+     */
+    private String userName;
+
+    /**
+     * db name
+     */
+    private String dbName;
+
+    public SqlParam() {}
+
+    public SqlParam(String userName, String dbName) {
+        this.userName = userName;
+        this.dbName = dbName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+}
