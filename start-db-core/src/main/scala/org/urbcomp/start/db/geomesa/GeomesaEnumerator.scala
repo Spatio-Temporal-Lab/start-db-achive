90bdf13fb29509379a8f81f8770da17178faf31d
beecorn <bohong_wang@hotmail.com>
2022-06-22 14:28:45 +0800

    delete executor

--- start-db-core/src/main/scala/org/urbcomp/start/db/geomesa/GeomesaEnumerator.scala
+++ start-db-core/src/main/scala/org/urbcomp/start/db/geomesa/GeomesaEnumerator.scala
@@ -16,13 +16,13 @@ import org.geotools.data.{DataStoreFinder, FeatureReader, Query, Transaction}
 import org.geotools.filter.text.ecql.ECQL
 import org.opengis.feature.simple.{SimpleFeature, SimpleFeatureType}
 import org.urbcomp.start.db.common.ConfigProvider
-import org.urbcomp.start.db.transformer.{
-  RoadSegmentAndGeomesaTransformer,
-  TrajectoryAndFeatureTransformer
-}
+import org.urbcomp.start.db.metadata.AccessorFactory
+import org.urbcomp.start.db.metadata.entity.Field
+import org.urbcomp.start.db.transformer.{RoadSegmentAndGeomesaTransformer, TrajectoryAndFeatureTransformer}
 import org.urbcomp.start.db.util.MetadataUtil
 
 import java.util
+import java.util.{Collections, Comparator}
 import scala.collection.JavaConverters._
 
 /**
@@ -32,7 +32,7 @@ import scala.collection.JavaConverters._
   * @author zaiyuan
   * @since 0.1.0
   */
-class GeomesaEnumerator(reader: FeatureReader[SimpleFeatureType, SimpleFeature])
+class GeomesaEnumerator(reader: FeatureReader[SimpleFeatureType, SimpleFeature], tableName: String, dbName: String, userName: String)
     extends Enumerator[Object] {
 
   var curr: Array[AnyRef] = _
@@ -44,16 +44,43 @@ class GeomesaEnumerator(reader: FeatureReader[SimpleFeatureType, SimpleFeature])
       if (!reader.hasNext) {
         return false
       }
-      val roadTransformer = new RoadSegmentAndGeomesaTransformer
-      val trajTransformer = new TrajectoryAndFeatureTransformer
-      val name = reader.getFeatureType.getName
-      if ("t_road_segment_test".equals(name.toString)) {
-        tempRoadSegmentMoveNext(roadTransformer)
-      } else if ("t_trajectory_test".equals(name.toString)) {
-        tempTrajectoryMoveNext(trajTransformer)
-      } else {
-        curr = reader.next().getAttributes.asScala.toArray
+      // sf index
+      var index = 0
+      val userAccessor = AccessorFactory.getUserAccessor
+      val dbAccessor = AccessorFactory.getDatabaseAccessor
+      val tableAccessor = AccessorFactory.getTableAccessor
+      val fieldAccessor = AccessorFactory.getFieldAccessor
+      val user = userAccessor.selectByFidAndName(1L /* never used*/ , userName, true)
+      if (user == null) return false
+      val database = dbAccessor.selectByFidAndName(user.getId, dbName, true)
+      if (database == null) return false
+      val table = tableAccessor.selectByFidAndName(database.getId, dbName, true)
+      if (table == null) return false
+      val fields = fieldAccessor.selectAllByFid(table.getId, true)
+      Collections.sort(fields, new Comparator[Field] {
+        override def compare(o1: Field, o2: Field): Int = (o1.getId - o2.getId).asInstanceOf[Int]
+      })
+      if (fields == null) return false
+      fields.toArray().asInstanceOf[Array[Field]]
+      val feature = reader.next()
+      val array = feature.getAttributes.asScala.toArray
+      val list = new util.ArrayList[AnyRef]
+      for (i <- 0 until fields.size()) {
+        val field = fields.get(i)
+        if (equals(field.getType) == "class org.urbcomp.start.db.model.roadnetwork.RoadSegment") {
+          val transformer = new RoadSegmentAndGeomesaTransformer
+          list.add(transformer.toRoadSegment(feature, field.getName))
+          index += 3
+        } else if (equals(field.getType) == "class org.urbcomp.start.db.model.trajectory.Trajectory") {
+          val transformer = new TrajectoryAndFeatureTransformer
+          list.add(transformer.toTrajectory(feature, field.getName))
+          index += 6
+        } else {
+          list.add(array(index))
+          index += 1
+        }
       }
+      curr = list.toArray
     } catch {
       case _: Exception => return false
     }
@@ -64,37 +91,37 @@ class GeomesaEnumerator(reader: FeatureReader[SimpleFeatureType, SimpleFeature])
 
   override def close(): Unit = reader.close()
 
-  /**
-    * Temporary iteration method for He Xiang's RoadSegment test table
-    * @author Wang Bohong
-    * @param transformer RoadSegmentAndGeomesaTransformer instance
-    */
-  private def tempRoadSegmentMoveNext(transformer: RoadSegmentAndGeomesaTransformer): Unit = {
-    val feature = reader.next()
-    val array = feature.getAttributes.asScala.toArray
-    val list = new util.ArrayList[AnyRef]
-    // a 行数据
-    list.add(array(0))
-    list.add(transformer.toRoadSegment(feature, "b"))
-    list.add(transformer.toRoadSegment(feature, "c"))
-    curr = list.toArray
-  }
-
-  /**
-    * Temporary iteration method for Li Zheng's Trajectory test table
-    * @author Wang Bohong
-    * @param transformer TrajectoryAndFeatureTransformer instance
-    */
-  private def tempTrajectoryMoveNext(transformer: TrajectoryAndFeatureTransformer): Unit = {
-    val feature = reader.next()
-    val array = feature.getAttributes.asScala.toArray
-    val list = new util.ArrayList[AnyRef]
-    // a 行数据
-    list.add(array(0))
-    list.add(transformer.toTrajectory(feature, "t1"))
-    curr = list.toArray
-  }
-}
+//  /**
+//    * Temporary iteration method for He Xiang's RoadSegment test table
+//    * @author Wang Bohong
+//    * @param transformer RoadSegmentAndGeomesaTransformer instance
+//    */
+//  private def tempRoadSegmentMoveNext(transformer: RoadSegmentAndGeomesaTransformer): Unit = {
+//    val feature = reader.next()
+//    val array = feature.getAttributes.asScala.toArray
+//    val list = new util.ArrayList[AnyRef]
+//    // a 行数据
+//    list.add(array(0))
+//    list.add(transformer.toRoadSegment(feature, "b"))
+//    list.add(transformer.toRoadSegment(feature, "c"))
+//    curr = list.toArray
+//  }
+//
+//  /**
+//    * Temporary iteration method for Li Zheng's Trajectory test table
+//    * @author Wang Bohong
+//    * @param transformer TrajectoryAndFeatureTransformer instance
+//    */
+//  private def tempTrajectoryMoveNext(transformer: TrajectoryAndFeatureTransformer): Unit = {
+//    val feature = reader.next()
+//    val array = feature.getAttributes.asScala.toArray
+//    val list = new util.ArrayList[AnyRef]
+//    // a 行数据
+//    list.add(array(0))
+//    list.add(transformer.toTrajectory(feature, "t1"))
+//    curr = list.toArray
+//  }
+//}
 
 object GeomesaEnumerator {
   def apply(filter: String, tableName: String, dbName: String, user: String): GeomesaEnumerator = {
@@ -102,6 +129,6 @@ object GeomesaEnumerator {
     val dataStore =
       DataStoreFinder.getDataStore(ConfigProvider.getGeomesaHbaseParam(catalog).asJava)
     val query = new Query(tableName, ECQL.toFilter(filter))
-    new GeomesaEnumerator(dataStore.getFeatureReader(query, Transaction.AUTO_COMMIT))
+    new GeomesaEnumerator(dataStore.getFeatureReader(query, Transaction.AUTO_COMMIT), tableName, user, dbName)
   }
 }
--- start-db-core/src/main/scala/org/urbcomp/start/db/geomesa/GeomesaTable.scala
+++ start-db-core/src/main/scala/org/urbcomp/start/db/geomesa/GeomesaTable.scala
@@ -17,15 +17,21 @@ import org.apache.calcite.plan.RelOptTable
 import org.apache.calcite.rel.RelNode
 import org.apache.calcite.rel.`type`.{RelDataType, RelDataTypeFactory}
 import org.apache.calcite.schema.{SchemaPlus, TranslatableTable}
+import org.geojson.{GeometryCollection, MultiPoint}
 import org.geotools.data.{DataStoreFinder, Query}
+import org.locationtech.jts.geom.{Geometry, LineString, MultiLineString, MultiPolygon, Point, Polygon}
 import org.urbcomp.start.db.common.ConfigProvider
 import org.urbcomp.start.db.geomesa.rel.GeomesaTableScan
+import org.urbcomp.start.db.metadata.AccessorFactory
+import org.urbcomp.start.db.metadata.entity.Field
 import org.urbcomp.start.db.model.roadnetwork.RoadSegment
 import org.urbcomp.start.db.model.trajectory.Trajectory
 import org.urbcomp.start.db.util.MetadataUtil
 
 import java.lang.reflect.Type
+import java.util.{Collections, Comparator}
 import scala.collection.JavaConverters._
+
 
 /**
   * Table of Geomesa
@@ -65,48 +71,97 @@ case class GeomesaTable(userName: String, dbName: String, tableName: String)
     * @return RelDataType
     */
   override def getRowType(relDataTypeFactory: RelDataTypeFactory): RelDataType = {
-    // ToDO： Temporary operation
-    if ("t_road_segment_test".equals(tableName))
-      return tempRoadSegmentGetRowType(relDataTypeFactory)
-    if ("t_trajectory_test".equals(tableName)) return tempTrajectoryGetRowType(relDataTypeFactory)
-    val catalog = MetadataUtil.makeCatalog(userName, dbName)
-    val dataStore =
-      DataStoreFinder.getDataStore(ConfigProvider.getGeomesaHbaseParam(catalog).asJava)
-    val query = new Query(tableName)
-    val sft = dataStore.getSchema(query.getTypeName)
-    // TODO Geometry type should be supported
+
+//    val catalog = MetadataUtil.makeCatalog(userName, dbName)
+//    val dataStore =
+//      DataStoreFinder.getDataStore(ConfigProvider.getGeomesaHbaseParam(catalog).asJava)
+//    val query = new Query(tableName)
+//    val sft = dataStore.getSchema(query.getTypeName)
+//
+//    val builder = relDataTypeFactory.builder()
+//    sft.getDescriptors.asScala.foreach { i =>
+//      builder.add(i.getName.toString, relDataTypeFactory.createJavaType(i.getType.getBinding))
+//    }
+//    dataStore.dispose()
+//    builder.build()
     val builder = relDataTypeFactory.builder()
-    sft.getDescriptors.asScala.foreach { i =>
-      builder.add(i.getName.toString, relDataTypeFactory.createJavaType(i.getType.getBinding))
+    val fields = metadataVerify()
+    if (fields != null) {
+      for (elem <- fields) {
+        builder.add(elem.getName, relDataTypeFactory.createJavaType(getClass(elem.getType)))
+      }
     }
-    dataStore.dispose()
     builder.build()
   }
 
-  /**
-    * Temporary method for He Xiang's RoadSegment test table
-    * @author Wang Bohong
-    * @param relDataTypeFactory  RelDataTypeFactory intance
-    * @return RelDataType instance
-    */
-  private def tempRoadSegmentGetRowType(relDataTypeFactory: RelDataTypeFactory): RelDataType = {
-    val builder = relDataTypeFactory.builder()
-    builder.add("a", relDataTypeFactory.createJavaType(Integer.TYPE))
-    builder.add("b", relDataTypeFactory.createJavaType(classOf[RoadSegment]))
-    builder.add("c", relDataTypeFactory.createJavaType(classOf[RoadSegment]))
-    builder.build()
+//  /**
+//    * Temporary method for He Xiang's RoadSegment test table
+//    * @author Wang Bohong
+//    * @param relDataTypeFactory  RelDataTypeFactory intance
+//    * @return RelDataType instance
+//    */
+//  private def tempRoadSegmentGetRowType(relDataTypeFactory: RelDataTypeFactory): RelDataType = {
+//    val builder = relDataTypeFactory.builder()
+//    builder.add("a", relDataTypeFactory.createJavaType(Integer.TYPE))
+//    builder.add("b", relDataTypeFactory.createJavaType(classOf[RoadSegment]))
+//    builder.add("c", relDataTypeFactory.createJavaType(classOf[RoadSegment]))
+//    builder.build()
+//  }
+//
+//  /**
+//    * Temporary method for He Xiang's RoadSegment test table
+//    * @author Wang Bohong
+//    * @param relDataTypeFactory  RelDataTypeFactory intance
+//    * @return RelDataType instance
+//    */
+//  private def tempTrajectoryGetRowType(relDataTypeFactory: RelDataTypeFactory): RelDataType = {
+//    val builder = relDataTypeFactory.builder()
+//    builder.add("id1", relDataTypeFactory.createJavaType(Integer.TYPE))
+//    builder.add("t1", relDataTypeFactory.createJavaType(classOf[Trajectory]))
+//    builder.build()
+//  }
+
+  def metadataVerify (): Array[Field] = {
+    val userAccessor = AccessorFactory.getUserAccessor
+    val dbAccessor = AccessorFactory.getDatabaseAccessor
+    val tableAccessor = AccessorFactory.getTableAccessor
+    val fieldAccessor = AccessorFactory.getFieldAccessor
+    val user = userAccessor.selectByFidAndName(1L /* never used*/ , userName, true)
+    if (user == null) return null
+    val database = dbAccessor.selectByFidAndName(user.getId, dbName, true)
+    if (database == null) return null
+    val table = tableAccessor.selectByFidAndName(database.getId, dbName, true)
+    if (table == null) return null
+    val fields = fieldAccessor.selectAllByFid(table.getId, true)
+    if (fields == null) return null
+    Collections.sort(fields, new Comparator[Field] {
+      override def compare(o1: Field, o2: Field): Int = (o1.getId - o2.getId).asInstanceOf[Int]
+    })
+    fields.toArray().asInstanceOf[Array[Field]]
   }
 
-  /**
-    * Temporary method for He Xiang's RoadSegment test table
-    * @author Wang Bohong
-    * @param relDataTypeFactory  RelDataTypeFactory intance
-    * @return RelDataType instance
-    */
-  private def tempTrajectoryGetRowType(relDataTypeFactory: RelDataTypeFactory): RelDataType = {
-    val builder = relDataTypeFactory.builder()
-    builder.add("id1", relDataTypeFactory.createJavaType(Integer.TYPE))
-    builder.add("t1", relDataTypeFactory.createJavaType(classOf[Trajectory]))
-    builder.build()
+  def getClass (dataType: String): Class[_ <: AnyRef] = {
+    dataType match {
+      case "class org.locationtech.jts.geom.Point"  => return classOf[Point]
+      case "class org.locationtech.jts.geom.Polygon"  => return classOf[Polygon]
+      case "class org.locationtech.jts.geom.LineString" => return classOf[LineString]
+      case "class org.geojson.MultiPoint" => return classOf[MultiPoint]
+      case "class org.locationtech.jts.geom.MultiLineString" => return classOf[MultiLineString]
+      case "class org.locationtech.jts.geom.MultiPolygon" => return classOf[MultiPolygon]
+      case "class org.geojson.GeometryCollection" => return classOf[GeometryCollection]
+      case "class org.locationtech.jts.geom.Geometry" => return classOf[Geometry]
+      case "class org.urbcomp.start.db.model.roadnetwork.RoadSegment" => return classOf[RoadSegment]
+      case "class org.urbcomp.start.db.model.trajectory.Trajectory" => return classOf[Trajectory]
+      case "class java.lang.Integer" => return classOf[java.lang.Integer]
+      case "class java.lang.Long" => return classOf[java.lang.Long]
+      case "class java.lang.Float" => return classOf[java.lang.Float]
+      case "class java.lang.Double" => return classOf[java.lang.Double]
+      case "class java.lang.String" => return classOf[java.lang.String]
+      case "class java.lang.Integer" => return classOf[java.lang.Boolean]
+      case "interface java.sql.Blob" => return classOf[java.sql.Blob]
+      case "class java.sql.Timestamp" => return classOf[java.sql.Blob]
+      case "class java.sql.Date" => return classOf[java.sql.Date]
+    }
+    null
   }
 }
