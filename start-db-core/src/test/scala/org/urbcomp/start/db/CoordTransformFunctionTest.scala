4f83ab8fb98776884221c0a073b4b6ba8f5c28c0
LIIIz <1519316637@qq.com>
2022-06-24 11:54:47 +0800

    CoordTransformFunction

--- start-db-core/src/test/scala/org/urbcomp/start/db/CoordTransformFunctionTest.scala
+++ start-db-core/src/test/scala/org/urbcomp/start/db/CoordTransformFunctionTest.scala
@@ -35,19 +35,6 @@ class CoordTransformFunctionTest extends AbstractCalciteFunctionTest {
     )
   }
 
-  test("st_BD09ToWGS84()") {
-    val statement = connect.createStatement
-    val resultSet = statement.executeQuery(
-      "select st_BD09ToWGS84(st_lineStringFromWKT('LINESTRING(0 0,1 1,1 2)'))"
-    )
-    resultSet.next()
-    assertEquals(
-      "LINESTRING (-0.0065045583144138 -0.0060041736071982," +
-        " 0.9934856269655111 0.9939867065696584, 0.9935048779206697 1.9940125213262534)",
-      resultSet.getObject(1).toString
-    )
-  }
-
   test("st_BD09ToWGS84(polygon)") {
     val statement = connect.createStatement
     val resultSet = statement.executeQuery(
@@ -60,6 +47,20 @@ class CoordTransformFunctionTest extends AbstractCalciteFunctionTest {
         " 0.9934856269655111 0.9939867065696584), (1.9935049129738731 1.9940140117465595," +
         " 1.9934965952780361 2.9940096919432193, 2.993512836601552 2.9939959051089304," +
         " 2.9935167767580073 1.9939989536125435, 1.9935049129738731 1.9940140117465595))",
+      resultSet.getObject(1).toString
+    )
+  }
+
+  test("st_BD09ToWGS84(MultiLineString)") {
+    val statement = connect.createStatement
+    val resultSet = statement.executeQuery(
+      "select st_BD09ToWGS84(st_mLineStringFromWKT('MULTILINESTRING((3 4,1 5,2 5),(-5 -8,-10 -8,-15 -4))'))"
+    )
+    resultSet.next()
+    assertEquals(
+      "MULTILINESTRING ((2.9934995619203466 3.993975880501857, 0.9934998808909472 4.994013668556448," +
+        " 1.9934935689438622 4.994016976565128), (-5.006485250634924 -8.00598615029063, " +
+        "-10.006466461068063 -8.006010605327319, -15.00652425844692 -4.005961014330163))",
       resultSet.getObject(1).toString
     )
   }
