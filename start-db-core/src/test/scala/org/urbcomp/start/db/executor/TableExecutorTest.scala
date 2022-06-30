a300e924feda9f75a73fb6f1267ad0f6d316311d
cquhx <48610039+cquhx@users.noreply.github.com>
2022-06-30 11:01:54 +0800

    add drop field function

--- start-db-core/src/test/scala/org/urbcomp/start/db/executor/TableExecutorTest.scala
+++ start-db-core/src/test/scala/org/urbcomp/start/db/executor/TableExecutorTest.scala
@@ -53,34 +53,33 @@ class TableExecutorTest extends AbstractCalciteFunctionTest {
   }
 
   test("test drop table") {
-//    val randomNum = scala.util.Random.nextInt(100000)
-    val randomNum = 8979
-//    val createTableSQL = s"""CREATE TABLE xxx_%d (
-//                            |    idx Integer,
-//                            |    ride_id String,
-//                            |    x1 String,
-//                            |    x2 String
-//                            |);""".format(randomNum).stripMargin
+    val randomNum = scala.util.Random.nextInt(100000)
+    val createTableSQL = s"""CREATE TABLE xxx_%d (
+                            |    idx Integer,
+                            |    ride_id String,
+                            |    x1 String,
+                            |    x2 String
+                            |);""".format(randomNum).stripMargin
     val stmt = connect.createStatement()
 
-//    val rs1 = stmt.executeQuery("show tables")
-//    val tablesBefore = ArrayBuffer[String]()
-//    while (rs1.next()) {
-//      tablesBefore += rs1.getString(1)
-//    }
+    val rs1 = stmt.executeQuery("show tables")
+    val tablesBefore = ArrayBuffer[String]()
+    while (rs1.next()) {
+      tablesBefore += rs1.getString(1)
+    }
 
-//    stmt.executeUpdate(createTableSQL)
+    stmt.executeUpdate(createTableSQL)
 
-    val dropTableSQL = s"""DROP TABLE start_db_table_test_%d;""".format(randomNum).stripMargin
+    val dropTableSQL = s"""DROP TABLE xxx_%d;""".format(randomNum).stripMargin
     stmt.executeUpdate(dropTableSQL)
 
-//    val rs2 = stmt.executeQuery("show tables")
-//    val tablesAfter = ArrayBuffer[String]()
-//    while (rs2.next()) {
-//      tablesAfter += rs2.getString(1)
-//    }
+    val rs2 = stmt.executeQuery("show tables")
+    val tablesAfter = ArrayBuffer[String]()
+    while (rs2.next()) {
+      tablesAfter += rs2.getString(1)
+    }
 
-//    assertEquals(tablesBefore.sorted, tablesAfter.sorted)
+    assertEquals(tablesBefore.sorted, tablesAfter.sorted)
   }
 
   test("test drop table if exists") {
