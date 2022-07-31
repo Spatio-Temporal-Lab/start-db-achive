6aff0d6c03cf794639c76b82e71f9c960f41f51e
beecorn <bohong_wang@hotmail.com>
2022-07-31 18:00:08 +0800

    将udf测试、crud测试放在不同的包下 重新整理dml dql 相关测试，测试表改为通过sql语句创建、测试完成后删表

--- start-db-core/src/test/scala/org/urbcomp/start/db/AbstractCalciteCRUDTest.scala
+++ start-db-core/src/test/scala/org/urbcomp/start/db/AbstractCalciteCRUDTest.scala
@@ -1,3 +1,14 @@
+/*
+ * Copyright 2022 ST-Lab
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
 package org.urbcomp.start.db
 
 import org.urbcomp.start.db.model.roadnetwork.RoadSegment
@@ -7,10 +18,10 @@ import org.urbcomp.start.db.model.trajectory.Trajectory
 import java.sql.Statement
 
 /**
- * test for DML & DQL
- * @author Wang Bohong
- * @Date 2022-07-22
- */
+  * test for DML & DQL
+  * @author Wang Bohong
+  * @Date 2022-07-22
+  */
 class AbstractCalciteCRUDTest extends AbstractCalciteFunctionTest {
 
   var statement: Statement = _
@@ -24,7 +35,9 @@ class AbstractCalciteCRUDTest extends AbstractCalciteFunctionTest {
   override protected def beforeAll(): Unit = {
     super.beforeAll()
     statement = connect.createStatement()
-    statement.executeUpdate("create table if not exists t_crud_test (idx Integer, rs RoadSegment, traj Trajectory)")
+    statement.executeUpdate(
+      "create table if not exists t_crud_test (idx Integer, rs RoadSegment, traj Trajectory)"
+    )
   }
 
   override protected def afterAll(): Unit = {
--- start-db-core/src/test/scala/org/urbcomp/start/db/executor/DMLTest.scala
+++ start-db-core/src/test/scala/org/urbcomp/start/db/executor/DMLTest.scala
@@ -1,25 +1,39 @@
+/*
+ * Copyright 2022 ST-Lab
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
 package org.urbcomp.start.db.executor
 
 import org.junit.Assert.assertEquals
 import org.urbcomp.start.db.AbstractCalciteCRUDTest
 
 /**
- * test for DML
- * @author Wang Bohong
- * @Date 2022-07-22
- */
-class DMLTest extends AbstractCalciteCRUDTest{
-
+  * test for DML
+  * @author Wang Bohong
+  * @Date 2022-07-22
+  */
+class DMLTest extends AbstractCalciteCRUDTest {
 
   /**
-   * insert one piece of data test and delete test
-   */
+    * insert one piece of data test and delete test
+    */
   test("basic insert and delete") {
     // insert into t (a, b, c) values (1, 2, 3)
-    val i1 = statement.executeUpdate("insert into t_crud_test (idx, rs, traj) values (1, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'), st_traj_fromGeoJSON(\'" + tGeo + "\'))")
+    val i1 = statement.executeUpdate(
+      "insert into t_crud_test (idx, rs, traj) values (1, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'), st_traj_fromGeoJSON(\'" + tGeo + "\'))"
+    )
     assertEquals(1, i1)
     // insert into t (a, b) values (1, 2)
-    val i2 = statement.executeUpdate("insert into t_crud_test values (2, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'), st_traj_fromGeoJSON(\'" + tGeo + "\'))")
+    val i2 = statement.executeUpdate(
+      "insert into t_crud_test values (2, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'), st_traj_fromGeoJSON(\'" + tGeo + "\'))"
+    )
     assertEquals(1, i2)
     val resultSet = statement.executeQuery("select count(1) from t_crud_test")
     resultSet.next()
@@ -36,26 +50,34 @@ class DMLTest extends AbstractCalciteCRUDTest{
   }
 
   /**
-   * insert multiple pieces of data test
-   */
+    * insert multiple pieces of data test
+    */
   test("multiple insert") {
     // insert into t (a, b, c) values (1, 2, 3), (4, 5, 6)
-    val i1 = statement.executeUpdate("insert into t_crud_test (idx, rs, traj) values (1, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'), st_traj_fromGeoJSON(\'" + tGeo + "\'))," +
-      "(2, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'), st_traj_fromGeoJSON(\'" + tGeo + "\'))")
+    val i1 = statement.executeUpdate(
+      "insert into t_crud_test (idx, rs, traj) values (1, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'), st_traj_fromGeoJSON(\'" + tGeo + "\'))," +
+        "(2, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'), st_traj_fromGeoJSON(\'" + tGeo + "\'))"
+    )
     assertEquals(2, i1)
     // insert into t (a, b) values (1, 2), (3, 4)
-    val i2 = statement.executeUpdate("insert into t_crud_test (idx, rs) values (3, st_rs_fromGeoJSON(\'" + rsGeoJson + "\')), (4, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'))")
+    val i2 = statement.executeUpdate(
+      "insert into t_crud_test (idx, rs) values (3, st_rs_fromGeoJSON(\'" + rsGeoJson + "\')), (4, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'))"
+    )
     assertEquals(2, i2)
     val i3 = statement.executeUpdate("delete from t_crud_test where 1 = 1")
     assertEquals(4, i3)
   }
 
   /**
-   * update data test
-   */
-  test ("update test") {
-    statement.executeUpdate("insert into t_crud_test (idx, rs, traj) values (1, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'), st_traj_fromGeoJSON(\'" + tGeo + "\'))")
-    statement.executeUpdate("insert into t_crud_test values (2, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'), st_traj_fromGeoJSON(\'" + tGeo + "\'))")
+    * update data test
+    */
+  test("update test") {
+    statement.executeUpdate(
+      "insert into t_crud_test (idx, rs, traj) values (1, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'), st_traj_fromGeoJSON(\'" + tGeo + "\'))"
+    )
+    statement.executeUpdate(
+      "insert into t_crud_test values (2, st_rs_fromGeoJSON(\'" + rsGeoJson + "\'), st_traj_fromGeoJSON(\'" + tGeo + "\'))"
+    )
     val i1 = statement.executeUpdate("update t_crud_test set idx = 3 where idx = 1")
     assertEquals(1, i1)
     val resultSet0 = statement.executeQuery("select count(1) from t_crud_test where idx = 3")
