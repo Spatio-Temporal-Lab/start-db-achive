40ca1fbb21b9749e074d362477aa767aff7e1a66
zaiyuan49 <zhugeliang0511>
2022-09-28 22:19:50 +0800

    spark sql 的 demo

--- start-db-core/src/test/scala/org/urbcomp/start/db/spark/SparkDemo.scala
+++ start-db-core/src/test/scala/org/urbcomp/start/db/spark/SparkDemo.scala
@@ -1,13 +1,25 @@
+/*
+ * Copyright 2022 ST-Lab
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
 package org.urbcomp.start.db.spark
 
 import org.apache.spark.sql.SparkSession
 
 object SparkDemo {
 
-  val ST_DIVIDE:      Int => Int       = g => g / 3
+  val ST_DIVIDE: Int => Int = g => g / 3
 
   def main(args: Array[String]): Unit = {
-    val sparkSession: SparkSession = SparkSession.builder()
+    val sparkSession: SparkSession = SparkSession
+      .builder()
       .appName("spark demo")
       .master("local[*]")
       .getOrCreate()
