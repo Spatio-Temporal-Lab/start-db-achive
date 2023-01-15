c8bb60f54a38f1987b8a5988b8f4d2a6063c26b9
zsc347 <zsc347@163.com>
2023-01-15 20:43:38 +0800

    fix gnu license header

--- start-db-common/src/test/java/org/urbcomp/start/db/model/RoadSegmentTest.java
+++ start-db-common/src/test/java/org/urbcomp/start/db/model/RoadSegmentTest.java
@@ -2,7 +2,7 @@
  * Copyright 2022 ST-Lab
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License version 2 as published by the Free Software Foundation.
+ * GNU General Public License version 3 as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
