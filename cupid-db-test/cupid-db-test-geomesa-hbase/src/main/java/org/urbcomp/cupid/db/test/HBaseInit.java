25f61d360456325e470cecc6f787e98cc283c35f
zcc0de <zechao.chen@kangry.net>
2023-02-08 17:40:27 +0800

    Add feature: HBase init.

---
+++ cupid-db-test/cupid-db-test-geomesa-hbase/src/main/java/org/urbcomp/cupid/db/test/HBaseInit.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 ST-Lab
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 3 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
+package org.urbcomp.cupid.db.test;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+
+public class HBaseInit {
+    public static void main(String[] args) throws Exception {
+        init();
+    }
+    public static void init() throws Exception {
+        Configuration config = HBaseConfiguration.create();
+        config.set("hbase.zookeeper.quorum", "localhost");
+        config.set("hbase.zookeeper.property.clientPort", "2181");
+
+        Connection conn = ConnectionFactory.createConnection(config);
+        Admin admin = conn.getAdmin();
+        TableName[] tableNames = admin.listTableNames();
+        for (TableName tableName : tableNames) {
+            admin.disableTable(tableName);
+            admin.deleteTable(tableName);
+        }
+        admin.close();
+        conn.close();
+    }
+}
