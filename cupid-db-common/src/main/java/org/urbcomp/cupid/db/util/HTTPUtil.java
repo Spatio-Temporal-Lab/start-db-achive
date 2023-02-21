0c31ccb9d72b089393ecccf24efccfbb78022c30
jimolonely <wp147188@foxmail.com>
2023-02-21 22:20:07 +0800

    add http util test

--- cupid-db-common/src/main/java/org/urbcomp/cupid/db/util/HTTPUtil.java
+++ cupid-db-common/src/main/java/org/urbcomp/cupid/db/util/HTTPUtil.java
@@ -11,17 +11,90 @@
 
 package org.urbcomp.cupid.db.util;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 /**
  * @author jimo
  **/
 public class HTTPUtil {
 
     public static String post(String url, String body) {
-        // TODO
-        return null;
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setEntity(new StringEntity(body, StandardCharsets.UTF_8));
+        final CloseableHttpClient httpClient = getClient();
+        CloseableHttpResponse response;
+        try {
+            response = httpClient.execute(httpPost);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != HttpStatus.SC_OK) {
+                httpPost.abort();
+                throw new RuntimeException("HttpClient,error status code :" + statusCode);
+            }
+            HttpEntity entity = response.getEntity();
+            String result = null;
+            if (entity != null) {
+                result = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+            }
+            EntityUtils.consume(entity);
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeClient(httpClient);
+        }
     }
 
     public static String get(String url) {
-        return null;
+        final CloseableHttpClient httpClient = getClient();
+        try {
+            HttpGet httpGet = new HttpGet(url);
+            CloseableHttpResponse response = httpClient.execute(httpGet);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                httpGet.abort();
+                throw new RuntimeException("HttpClient,error status code :" + statusCode);
+            }
+            HttpEntity entity = response.getEntity();
+            String result = null;
+            if (entity != null) {
+                result = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+            }
+            EntityUtils.consume(entity);
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeClient(httpClient);
+        }
+    }
+
+    private static CloseableHttpClient getClient() {
+        RequestConfig config = RequestConfig.custom()
+            .setConnectTimeout(60000)
+            .setSocketTimeout(15000)
+            .build();
+        return HttpClientBuilder.create().setDefaultRequestConfig(config).build();
+    }
+
+    private static void closeClient(CloseableHttpClient httpClient) {
+        if (httpClient != null) {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
