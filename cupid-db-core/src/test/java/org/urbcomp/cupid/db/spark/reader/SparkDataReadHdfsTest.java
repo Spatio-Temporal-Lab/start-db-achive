1988a5648c4f45fd5e96876a133d208c437688ef
jimolonely <wp147188@foxmail.com>
2023-03-12 11:18:33 +0800

    ignore test

--- cupid-db-core/src/test/java/org/urbcomp/cupid/db/spark/reader/SparkDataReadHdfsTest.java
+++ cupid-db-core/src/test/java/org/urbcomp/cupid/db/spark/reader/SparkDataReadHdfsTest.java
@@ -1,10 +1,13 @@
 package org.urbcomp.cupid.db.spark.reader;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.urbcomp.cupid.db.infra.MetadataResult;
 
 import static org.junit.Assert.assertNotNull;
 
+// 起Hadoop
+@Ignore
 public class SparkDataReadHdfsTest {
 
     @Test
