40aaa1f86526cd57a072c3d95d2c85558ec9176e
TYXXXXXXX <2237952894@qq.com>
2023-03-22 19:53:38 +0800

    MathFunction 解决输入小数报错问题，统一修改返回值类型为double或decimal或null,参数为的null问题

--- cupid-db-calcite/src/main/java/org/urbcomp/cupid/db/function/MathFunction.java
+++ cupid-db-calcite/src/main/java/org/urbcomp/cupid/db/function/MathFunction.java
@@ -25,12 +25,12 @@ package org.urbcomp.cupid.db.function;
 import java.math.BigDecimal;
 
 public class MathFunction {
+
     /**
      * The double value that is closer than any other to e,
      * the base of the natural logarithms.
      */
     public static final double E = 2.7182818284590452354;
-
     /**
      * The double value that is closer than any other to pi,
      * the ratio of the circumference of a circle to its diameter.
@@ -46,6 +46,11 @@ public class MathFunction {
      */
     @CupidDBFunction("log")
     public Object log(BigDecimal base, BigDecimal num) {
+        if (base == null
+            || num == null
+            || base.doubleValue() <= 0
+            || base.doubleValue() == 1
+            || num.doubleValue() == 0) return null;
         double res = Math.log(num.doubleValue()) / Math.log(base.doubleValue());
         if (res % 1 == 0) return res;
         return BigDecimal.valueOf(res);
@@ -69,6 +74,7 @@ public class MathFunction {
      */
     @CupidDBFunction("log2")
     public Object log2(BigDecimal num) {
+        if (num == null || num.doubleValue() <= 0) return null;
         double res = Math.log(num.doubleValue()) / Math.log(2.0D);
         if (res % 1 == 0) return res;
         return BigDecimal.valueOf(res);
@@ -82,6 +88,7 @@ public class MathFunction {
      */
     @CupidDBFunction("log1p")
     public Object log1p(BigDecimal num) {
+        if (num == null || num.doubleValue() <= 0) return null;
         double res = Math.log1p(num.doubleValue());
         if (res % 1 == 0) return res;
         return BigDecimal.valueOf(res);
@@ -96,6 +103,7 @@ public class MathFunction {
      */
     @CupidDBFunction("pow")
     public Object pow(BigDecimal a, BigDecimal b) {
+        if (a == null || b == null) return null;
         double res = Math.pow(a.doubleValue(), b.doubleValue());
         if (res % 1 == 0) return res;
         return BigDecimal.valueOf(res);
@@ -110,6 +118,7 @@ public class MathFunction {
      */
     @CupidDBFunction("toRadians")
     public Object toRadians(BigDecimal angDeg) {
+        if (angDeg == null) return null;
         double res = Math.toRadians(angDeg.doubleValue());
         if (res % 1 == 0) return res;
         return BigDecimal.valueOf(res);
@@ -125,7 +134,36 @@ public class MathFunction {
      */
     @CupidDBFunction("toDegrees")
     public Object toDegrees(BigDecimal angRad) {
+        if (angRad == null) return null;
         double res = Math.toDegrees(angRad.doubleValue());
+        if (res % 1 == 0) return res;
+        return BigDecimal.valueOf(res);
+    }
+
+    @CupidDBFunction("abs")
+    public Object abs(BigDecimal a) {
+        if (a == null) return null;
+        double res = Math.abs(a.doubleValue());
+        if (res % 1 == 0) return res;
+        return BigDecimal.valueOf(res);
+    }
+
+    @CupidDBFunction("floor")
+    public Double floor(BigDecimal a) {
+        if (a == null) return null;
+        return Math.floor(a.doubleValue());
+    }
+
+    @CupidDBFunction("ceil")
+    public Double ceil(BigDecimal a) {
+        if (a == null) return null;
+        return Math.ceil(a.doubleValue());
+    }
+
+    @CupidDBFunction("mod")
+    public Object mod(BigDecimal a, BigDecimal b) {
+        if (a == null || b == null) return null;
+        double res = a.doubleValue() % b.doubleValue();
         if (res % 1 == 0) return res;
         return BigDecimal.valueOf(res);
     }
--- cupid-db-core/src/test/scala/org/urbcomp/cupid/db/MathFunctionTest.scala
+++ cupid-db-core/src/test/scala/org/urbcomp/cupid/db/MathFunctionTest.scala
@@ -28,6 +28,10 @@ class MathFunctionTest extends AbstractCalciteFunctionTest {
     val resultSet = statement.executeQuery("select log(3.3,9.6)")
     resultSet.next()
     assertEquals(BigDecimal(1.8943969631190591).bigDecimal, resultSet.getObject(1))
+    val resultSet2 = statement.executeQuery("select log(null,null)")
+    resultSet2.next();
+    assertEquals(null, resultSet2.getObject(1))
+
   }
 
   /**
@@ -48,6 +52,9 @@ class MathFunctionTest extends AbstractCalciteFunctionTest {
     val resultSet = statement.executeQuery("select log1p(1.5)")
     resultSet.next()
     assertEquals(BigDecimal(0.9162907318741551).bigDecimal, resultSet.getObject(1))
+    val resultSet2 = statement.executeQuery("select log1p(null)")
+    resultSet2.next()
+    assertEquals(null, resultSet2.getObject(1))
   }
 
   /**
@@ -58,6 +65,9 @@ class MathFunctionTest extends AbstractCalciteFunctionTest {
     val resultSet = statement.executeQuery("select log2(8.6)")
     resultSet.next()
     assertEquals(BigDecimal(3.1043366598147353).bigDecimal, resultSet.getObject(1))
+    val resultSet2 = statement.executeQuery("select log2(null)")
+    resultSet2.next()
+    assertEquals(null, resultSet2.getObject(1))
   }
 
   /**
@@ -68,6 +78,10 @@ class MathFunctionTest extends AbstractCalciteFunctionTest {
     val resultSet = statement.executeQuery("select pow(0.5,3)")
     resultSet.next()
     assertEquals(BigDecimal(0.125).bigDecimal, resultSet.getObject(1))
+    val resultSet2 = statement.executeQuery("select pow(null,null)")
+    resultSet2.next()
+    assertEquals(null, resultSet2.getObject(1))
+
   }
 
   /**
@@ -78,6 +92,10 @@ class MathFunctionTest extends AbstractCalciteFunctionTest {
     val resultSet = statement.executeQuery("select toRadians(90)")
     resultSet.next()
     assertEquals(BigDecimal(1.5707963267948966).bigDecimal, resultSet.getObject(1))
+    val resultSet2 = statement.executeQuery("select toRadians(null)")
+    resultSet2.next()
+    assertEquals(null, resultSet2.getObject(1))
+
   }
 
   /**
@@ -88,5 +106,48 @@ class MathFunctionTest extends AbstractCalciteFunctionTest {
     val resultSet = statement.executeQuery("select toDegrees(1)")
     resultSet.next()
     assertEquals(BigDecimal(57.29577951308232).bigDecimal, resultSet.getObject(1))
+    val resultSet2 = statement.executeQuery("select toDegrees(null)")
+    resultSet2.next()
+    assertEquals(null, resultSet2.getObject(1))
+  }
+
+  test("abs") {
+    val statement = connect.createStatement()
+    val resultSet = statement.executeQuery("select abs(-1.9)")
+    resultSet.next()
+    assertEquals(BigDecimal(1.9).bigDecimal, resultSet.getObject(1))
+    val resultSet2 = statement.executeQuery("select abs(null)")
+    resultSet2.next()
+    assertEquals(null, resultSet2.getObject(1))
+  }
+
+  test("floor") {
+    val statement = connect.createStatement()
+    val resultSet = statement.executeQuery("select floor(1.9)")
+    resultSet.next()
+    assertEquals(1.0, resultSet.getObject(1))
+    val resultSet2 = statement.executeQuery("select floor(null)")
+    resultSet2.next()
+    assertEquals(null, resultSet2.getObject(1))
+  }
+
+  test("ceil") {
+    val statement = connect.createStatement()
+    val resultSet = statement.executeQuery("select ceil(1.9)")
+    resultSet.next()
+    assertEquals(2.0, resultSet.getObject(1))
+    val resultSet2 = statement.executeQuery("select ceil(null)")
+    resultSet2.next()
+    assertEquals(null, resultSet2.getObject(1))
+  }
+
+  test("mod") {
+    val statement = connect.createStatement()
+    val resultSet = statement.executeQuery("select mod(2.4,5)")
+    resultSet.next()
+    assertEquals(BigDecimal(2.4).bigDecimal, resultSet.getObject(1))
+    val resultSet2 = statement.executeQuery("select mod(null,null)")
+    resultSet2.next()
+    assertEquals(null, resultSet2.getObject(1))
   }
 }
